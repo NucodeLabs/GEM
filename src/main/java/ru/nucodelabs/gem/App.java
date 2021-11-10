7954360616fb95521dc6b7eb1f054506ba3b515f
Michael <m.plugin@g.nsu.ru>
2021-11-10 22:24:21 +0700

    Added Icon Pog

--- src/main/java/ru/nucodelabs/gem/App.java
+++ src/main/java/ru/nucodelabs/gem/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +23,9 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"), bundle);
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 1280, 720);
+
+        Image icon = new Image("nu.png");
+        primaryStage.getIcons().add(icon);
 
         primaryStage.setTitle("GEM - Main Window");
         primaryStage.setScene(scene);
