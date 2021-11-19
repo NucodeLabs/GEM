4df8ff918ff2a0a3724f2e31f56acc9770354413
Vadim <vadimmos2002@gmail.com>
2021-11-19 12:05:52 +0700

    Refactor, ves creating methods now independent from Sonet files, style edited

--- src/main/java/ru/nucodelabs/gem/ExperimentalCurve.java
+++ src/main/java/ru/nucodelabs/gem/ExperimentalCurve.java
@@ -2,54 +2,56 @@ package ru.nucodelabs.gem;
 
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-import ru.nucodelabs.files.sonet.EXPFile;
-import ru.nucodelabs.files.sonet.STTFile;
+
+import java.util.List;
 
 import static java.lang.Math.log10;
 import static java.lang.Math.max;
 
 public class ExperimentalCurve {
 
-    protected static void makeCurve(LineChart<Double, Double> vesCurve, STTFile openedSTT, EXPFile openedEXP) {
+    protected static void makeCurve(
+            LineChart<Double, Double> vesCurve,
+            List<Double> AB_2,
+            List<Double> resistanceApp,
+            List<Double> errResistanceApp) {
         vesCurve.getData().clear();
 
-        vesCurve.getData().add(makeCurveData(openedSTT, openedEXP));
-        vesCurve.getData().add(makeCurveErrorUpper(openedSTT, openedEXP));
-        vesCurve.getData().add(makeCurveErrorLower(openedSTT, openedEXP));
+        vesCurve.getData().add(makeCurveData(AB_2, resistanceApp));
+        vesCurve.getData().add(makeCurveErrorUpper(AB_2, resistanceApp, errResistanceApp));
+        vesCurve.getData().add(makeCurveErrorLower(AB_2, resistanceApp, errResistanceApp));
     }
 
-    private static XYChart.Series<Double, Double> makeCurveData(STTFile openedSTT, EXPFile openedEXP) {
+    private static XYChart.Series<Double, Double> makeCurveData(List<Double> AB_2, List<Double> resistanceApp) {
         XYChart.Series<Double, Double> pointsSeries = new XYChart.Series<>();
 
-        for (int i = 0; i < openedSTT.getAB_2().size(); i++) {
-            double dotX = log10(openedSTT.getAB_2().get(i));
-            double dotY = max(log10(openedEXP.getResistanceApp().get(i)), 0);
+        for (int i = 0; i < AB_2.size(); i++) {
+            double dotX = log10(AB_2.get(i));
+            double dotY = max(log10(resistanceApp.get(i)), 0);
             pointsSeries.getData().add(new XYChart.Data<>(dotX, dotY));
         }
         return pointsSeries;
     }
 
-    protected static XYChart.Series<Double, Double> makeCurveErrorUpper(STTFile openedSTT, EXPFile openedEXP) {
+    protected static XYChart.Series<Double, Double> makeCurveErrorUpper(List<Double> AB_2, List<Double> resistanceApp, List<Double> errorResistanceApp) {
         XYChart.Series<Double, Double> pointsSeries = new XYChart.Series<>();
 
-        for (int i = 0; i < openedSTT.getAB_2().size(); i++) {
-            double dotX = log10(openedSTT.getAB_2().get(i));
-            double resistance = openedEXP.getResistanceApp().get(i);
-            double error = openedEXP.getErrorResistanceApp().get(i) / 100f;
-            double dotY = max(log10(resistance + resistance * error), 0);
+        for (int i = 0; i < AB_2.size(); i++) {
+            double dotX = log10(AB_2.get(i));
+            double error = errorResistanceApp.get(i) / 100f;
+            double dotY = max(log10(resistanceApp.get(i) + resistanceApp.get(i) * error), 0);
             pointsSeries.getData().add(new XYChart.Data<>(dotX, dotY));
         }
         return pointsSeries;
     }
 
-    protected static XYChart.Series<Double, Double> makeCurveErrorLower(STTFile openedSTT, EXPFile openedEXP) {
+    protected static XYChart.Series<Double, Double> makeCurveErrorLower(List<Double> AB_2, List<Double> resistanceApp, List<Double> errorResistanceApp) {
         XYChart.Series<Double, Double> pointsSeries = new XYChart.Series<>();
 
-        for (int i = 0; i < openedSTT.getAB_2().size(); i++) {
-            double dotX = log10(openedSTT.getAB_2().get(i));
-            double resistance = openedEXP.getResistanceApp().get(i);
-            double error = openedEXP.getErrorResistanceApp().get(i) / 100f;
-            double dotY = max(log10(resistance - resistance * error), 0);
+        for (int i = 0; i < AB_2.size(); i++) {
+            double dotX = log10(AB_2.get(i));
+            double error = errorResistanceApp.get(i) / 100f;
+            double dotY = max(log10(resistanceApp.get(i) - resistanceApp.get(i) * error), 0);
             pointsSeries.getData().add(new XYChart.Data<>(dotX, dotY));
         }
         return pointsSeries;
--- src/main/java/ru/nucodelabs/gem/TheoreticalCurve.java
+++ src/main/java/ru/nucodelabs/gem/TheoreticalCurve.java
@@ -3,8 +3,6 @@ package ru.nucodelabs.gem;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
-import ru.nucodelabs.files.sonet.MODFile;
-import ru.nucodelabs.files.sonet.STTFile;
 
 import java.util.List;
 
@@ -12,15 +10,23 @@ import static java.lang.Math.log10;
 import static java.lang.Math.max;
 
 public class TheoreticalCurve {
-    protected static List<Double> solvedResistance;
+    private static List<Double> solvedResistance = null;
 
-    protected static void makeCurve(LineChart<Double, Double> vesCurve, STTFile openedSTT, MODFile openedMOD) {
+    public static List<Double> getSolvedResistance() {
+        return solvedResistance;
+    }
+
+    protected static void makeCurve(
+            LineChart<Double, Double> vesCurve,
+            List<Double> AB_2,
+            List<Double> resistance,
+            List<Double> power) {
         if (vesCurve.getData().size() > AppController.EXP_CURVE_SERIES_CNT) {
             vesCurve.getData().remove(AppController.EXP_CURVE_SERIES_CNT);
         }
 
         try {
-            solvedResistance = ForwardSolver.ves(openedMOD.getResistance(), openedMOD.getPower(), openedSTT.getAB_2());
+            solvedResistance = ForwardSolver.ves(resistance, power, AB_2);
         } catch (UnsatisfiedLinkError e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Невозможно решить прямую задачу");
@@ -34,14 +40,14 @@ public class TheoreticalCurve {
             alert.show();
             return;
         }
-        vesCurve.getData().add(makeCurveData(openedSTT, solvedResistance));
+        vesCurve.getData().add(makeCurveData(AB_2, solvedResistance));
     }
 
-    private static XYChart.Series<Double, Double> makeCurveData(STTFile openedSTT, List<Double> solvedResistance) {
+    private static XYChart.Series<Double, Double> makeCurveData(List<Double> AB_2, List<Double> solvedResistance) {
         XYChart.Series<Double, Double> pointsSeries = new XYChart.Series<>();
 
-        for (int i = 0; i < openedSTT.getAB_2().size(); i++) {
-            double dotX = log10(openedSTT.getAB_2().get(i));
+        for (int i = 0; i < AB_2.size(); i++) {
+            double dotX = log10(AB_2.get(i));
             double dotY = max(log10(solvedResistance.get(i)), 0);
             pointsSeries.getData().add(new XYChart.Data<>(dotX, dotY));
         }
