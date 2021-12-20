345bccce211da6c1a9eae6051b6c0bcced2c8434
Vadim <vadimmos2002@gmail.com>
2021-12-20 23:46:49 +0700

    Constructor body extracted to reuse-ability of code.

---
+++ src/main/java/ru/nucodelabs/mvvm/Initializers.java
@@ -0,0 +1,29 @@
+package ru.nucodelabs.mvvm;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Initializers {
+    /**
+     * Load FXML file with name equals to root class name and sets the root as the controller of loaded FXML.
+     *
+     * @param root object that inherits class of root node of FXML
+     * @param <N>  class of root
+     */
+    public static <N extends Node> void initFXMLControl(N root) {
+        ResourceBundle bundle = ResourceBundle.getBundle("ru/nucodelabs/gem/UI", new Locale("ru"));
+        String fxmlFileName = root.getClass().getSimpleName() + ".fxml";
+        FXMLLoader loader = new FXMLLoader(root.getClass().getResource(fxmlFileName), bundle);
+        loader.setRoot(root);
+        loader.setController(root);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
