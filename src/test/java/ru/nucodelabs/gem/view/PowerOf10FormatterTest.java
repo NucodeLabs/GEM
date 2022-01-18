b111863a86dff084f6690ce35139196212a65120
Vadim <vadimmos2002@gmail.com>
2022-01-18 18:52:09 +0700

    Some UI fixes

--- src/test/java/ru/nucodelabs/gem/view/PowerOf10FormatterTest.java
+++ src/test/java/ru/nucodelabs/gem/view/PowerOf10FormatterTest.java
@@ -1,14 +1,13 @@
-package ru.nucodelabs.gem;
+package ru.nucodelabs.gem.view;
 
 import org.junit.jupiter.api.Test;
-import ru.nucodelabs.gem.view.PowerOf10Formatter;
 
 import java.util.Arrays;
 
 public class PowerOf10FormatterTest {
     @Test
     void test() {
-        Number[] src = new Number[]{1.23, 4.56, 7.089};
+        Number[] src = new Number[]{1.23, 4.56, 7.089, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Arrays.stream(src).forEach(n -> System.out.println((new PowerOf10Formatter()).toString(n)));
     }
 }
