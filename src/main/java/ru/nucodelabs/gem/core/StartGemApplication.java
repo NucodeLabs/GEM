389e60e7593694261cc0f47fb4a3856c0388c1ed
Vadim <vadimmos2002@gmail.com>
2022-01-25 15:20:37 +0700

    upd

--- src/main/java/ru/nucodelabs/gem/core/StartGemApplication.java
+++ src/main/java/ru/nucodelabs/gem/core/StartGemApplication.java
@@ -7,8 +7,10 @@ import javafx.application.Application;
  */
 public class StartGemApplication {
     public static void main(String[] args) {
+        // only for debug!!!
         System.loadLibrary("forwardsolver"); // for faster first time file opening
         System.loadLibrary("misfit");
+        // because here is no error handling
 
         Application.launch(GemApplication.class);
     }
