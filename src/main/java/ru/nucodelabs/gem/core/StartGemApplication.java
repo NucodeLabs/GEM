433c22640248c87ec7851951547875c79bcde10c
Vadim <vadimmos2002@gmail.com>
2022-01-26 18:40:13 +0700

    upd

--- src/main/java/ru/nucodelabs/gem/core/StartGemApplication.java
+++ src/main/java/ru/nucodelabs/gem/core/StartGemApplication.java
@@ -2,14 +2,19 @@ package ru.nucodelabs.gem.core;
 
 import javafx.application.Application;
 
+import java.util.Arrays;
+
 /**
  * Starts the Application
  */
 public class StartGemApplication {
     public static void main(String[] args) {
         // only for debug!!!
-        System.loadLibrary("forwardsolver"); // for faster first time file opening
-        System.loadLibrary("misfit");
+        if (Arrays.asList(args).contains("--pre-load")) {
+            System.loadLibrary("forwardsolver"); // for faster first time file opening
+            System.loadLibrary("misfit");
+            System.out.println("--pre-load: DLL loaded.");
+        }
         // because here is no error handling
 
         Application.launch(GemApplication.class);
