be67a2dc2181f56c2dfa7c3187eed929a974886c
Vadim <vadimmos2002@gmail.com>
2022-01-27 15:27:04 +0700

    begin of new GUI layout

--- src/main/java/ru/nucodelabs/gem/view/main/MainSplitLayoutView.java
+++ src/main/java/ru/nucodelabs/gem/view/main/MainSplitLayoutView.java
@@ -1,38 +1,11 @@
 package ru.nucodelabs.gem.view.main;
 
-import javafx.fxml.FXML;
-import ru.nucodelabs.gem.view.usercontrols.mainmenubar.MainMenuBar;
-import ru.nucodelabs.gem.view.usercontrols.misfitstacks.MisfitStacks;
-import ru.nucodelabs.gem.view.usercontrols.vescurves.VESCurves;
-import ru.nucodelabs.gem.view.usercontrols.vesmisfitsplit.VESMisfitSplit;
 import ru.nucodelabs.mvvm.VBView;
 
 public class MainSplitLayoutView extends VBView<MainViewModel> {
-
-    @FXML
-    private MainMenuBar menuBar;
-    @FXML
-    private VESMisfitSplit vesMisfitSplit;
-
-
     public MainSplitLayoutView(MainViewModel viewModel) {
         super(viewModel);
-        viewModel.initModelCurveDragger(vesMisfitSplit.getVesCurves().getLineChart());
 
-        MisfitStacks misfitStacks = vesMisfitSplit.getMisfitStacks();
-        VESCurves vesCurves = vesMisfitSplit.getVesCurves();
 
-        menuBar.getMenuFileOpenEXP().setOnAction(e -> viewModel.importEXP());
-        menuBar.getMenuFileOpenMOD().setOnAction(e -> viewModel.importMOD());
-        menuBar.getMenuFileOpenMOD().disableProperty().bind(viewModel.menuFileMODDisabledProperty());
-        menuBar.getMenuViewLegendsVESCurves().selectedProperty().bindBidirectional(viewModel.vesLegendsVisibleProperty());
-
-        vesCurves.getLineChart().legendVisibleProperty().bind(viewModel.vesLegendsVisibleProperty());
-        vesCurves.getLineChart().dataProperty().bindBidirectional(viewModel.vesCurvesDataProperty());
-        vesCurves.getLineChart().visibleProperty().bind(viewModel.vesLineChartVisibleProperty());
-        vesCurves.getText().textProperty().bind(viewModel.vesTextProperty());
-
-        misfitStacks.getLineChart().visibleProperty().bind(viewModel.misfitStacksLineChartVisibleProperty());
-        misfitStacks.getLineChart().dataProperty().bind(viewModel.misfitStacksDataProperty());
     }
 }
--- src/main/java/ru/nucodelabs/gem/view/usercontrols/vescurves/VESCurves.java
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/vescurves/VESCurves.java
@@ -3,7 +3,6 @@ package ru.nucodelabs.gem.view.usercontrols.vescurves;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.text.Text;
 import ru.nucodelabs.gem.view.PowerOf10Formatter;
 import ru.nucodelabs.mvvm.VBUserControl;
 
@@ -11,9 +10,6 @@ import ru.nucodelabs.mvvm.VBUserControl;
  * VES Curves line chart with text above.
  */
 public class VESCurves extends VBUserControl {
-
-    @FXML
-    private Text text;
     @FXML
     private LineChart<Double, Double> lineChart;
     @FXML
@@ -25,11 +21,6 @@ public class VESCurves extends VBUserControl {
         super();
         lineChartXAxis.setTickLabelFormatter(new PowerOf10Formatter());
         lineChartYAxis.setTickLabelFormatter(new PowerOf10Formatter());
-    }
-
-
-    public Text getText() {
-        return text;
     }
 
     public LineChart<Double, Double> getLineChart() {
