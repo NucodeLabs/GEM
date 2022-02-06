c6f19606f0a7d910159dd082e3fa9f925a5e3e77
Vadim <vadimmos2002@gmail.com>
2022-02-06 20:06:45 +0700

    User controls refactor

--- src/main/java/ru/nucodelabs/gem/view/main/MainSplitLayoutView.java
+++ src/main/java/ru/nucodelabs/gem/view/main/MainSplitLayoutView.java
@@ -1,25 +1,31 @@
 package ru.nucodelabs.gem.view.main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import ru.nucodelabs.gem.view.usercontrols.mainmenubar.MainMenuBar;
 import ru.nucodelabs.gem.view.usercontrols.misfitstacks.MisfitStacks;
 import ru.nucodelabs.gem.view.usercontrols.vescurves.VESCurves;
-import ru.nucodelabs.gem.view.usercontrols.vescurvestablesplit.VESCurvesTableSplit;
 import ru.nucodelabs.mvvm.VBView;
 
 public class MainSplitLayoutView extends VBView<MainViewModel> {
     @FXML
     public MainMenuBar mainMenuBar;
     @FXML
-    public VESCurvesTableSplit vesCurvesTable;
+    public Button prevButton;
+    @FXML
+    public Text text;
+    @FXML
+    public Button nextButton;
+    @FXML
+    public MisfitStacks misfitStacks;
+    @FXML
+    public VESCurves vesCurves;
 
     public MainSplitLayoutView(MainViewModel viewModel) {
         super(viewModel);
 
-        viewModel.initModelCurveDragger(vesCurvesTable.getVesCurves().getLineChart());
-
-        MisfitStacks misfitStacks = vesCurvesTable.getMisfitStacks();
-        VESCurves vesCurves = vesCurvesTable.getVesCurves();
+        viewModel.initModelCurveDragger(vesCurves.getLineChart());
 
         mainMenuBar.getMenuFileOpenEXP().setOnAction(e -> viewModel.importEXP());
         mainMenuBar.getMenuFileOpenMOD().setOnAction(e -> viewModel.importMOD());
@@ -27,7 +33,7 @@ public class MainSplitLayoutView extends VBView<MainViewModel> {
         mainMenuBar.getMenuViewLegendsVESCurves().selectedProperty().bindBidirectional(vesCurves.getLineChart().legendVisibleProperty());
 
         vesCurves.getLineChart().dataProperty().bindBidirectional(viewModel.vesCurvesDataProperty());
-        vesCurvesTable.getText().textProperty().bind(viewModel.vesTextProperty());
+        text.textProperty().bind(viewModel.vesTextProperty());
 
         misfitStacks.getLineChart().visibleProperty().bind(viewModel.misfitStacksVisibleProperty());
         misfitStacks.getLineChart().dataProperty().bind(viewModel.misfitStacksDataProperty());
