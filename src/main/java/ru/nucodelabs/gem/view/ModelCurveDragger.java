edd469c62c16aa94f3bfb6913fb209409ac5db5d
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-02-10 11:52:03 +0700

    REALTIME CHANGES WHILE DRAGGING MODEL ADDED (for resistance only at the moment)

--- src/main/java/ru/nucodelabs/gem/view/ModelCurveDragger.java
+++ src/main/java/ru/nucodelabs/gem/view/ModelCurveDragger.java
@@ -1,18 +1,32 @@
 package ru.nucodelabs.gem.view;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.input.MouseEvent;
+import ru.nucodelabs.data.ves.ModelData;
+import ru.nucodelabs.gem.view.main.MainViewModel;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+
+import static java.lang.Math.log10;
+import static java.lang.Math.pow;
 
 public class ModelCurveDragger {
 
     private static final double TOLERANCE = 0.005;
     private final int MOD_CURVE_SERIES_INDEX = 4;
 
+    private MainViewModel viewModel;
     private LineChart<Double, Double> vesCurvesLineChart;
+    private ObjectProperty<ObservableList<XYChart.Series<Double, Double>>> vesCurvesData;
+    private ModelData modelData;
+    private Map<XYChart.Data<Double, Double>, Integer> pointResistanceMap;
+    private Map<XYChart.Data<Double, Double>, Integer> pointPowerMap;
 
     // ends of line to be dragged
     private XYChart.Data<Double, Double> point1;
@@ -22,8 +36,11 @@ public class ModelCurveDragger {
     private Double leftLimitX;
     private Double rightLimitX;
 
-    public ModelCurveDragger(LineChart<Double, Double> vesCurvesLineChart) {
+    public ModelCurveDragger(MainViewModel viewModel, LineChart<Double, Double> vesCurvesLineChart, ObjectProperty<ObservableList<XYChart.Series<Double, Double>>> vesCurvesData) {
+        this.viewModel = viewModel;
         this.vesCurvesLineChart = vesCurvesLineChart;
+        this.vesCurvesData = vesCurvesData;
+        pointResistanceMap = new HashMap<>();
     }
 
     public void lineToDragDetector(MouseEvent mouseEvent) {
@@ -33,7 +50,7 @@ public class ModelCurveDragger {
                 vesCurvesLineChart.getXAxis().sceneToLocal(pointInScene).getX()
         );
 
-        var points = vesCurvesLineChart.getData().get(MOD_CURVE_SERIES_INDEX).getData();
+        var points = vesCurvesData.get().get(MOD_CURVE_SERIES_INDEX).getData();
         var closestVerticalLines = points.stream()
                 .filter(p -> p.getXValue() > mouseX - TOLERANCE && p.getXValue() < mouseX + TOLERANCE)
                 .toList();
@@ -82,6 +99,26 @@ public class ModelCurveDragger {
             } else if (Objects.equals(point1.getYValue(), point2.getYValue())) {
                 point1.setYValue(mouseY);
                 point2.setYValue(mouseY);
+                modelData.getResistance().set(pointResistanceMap.get(point1), pow(10, point1.getYValue()));
+                viewModel.updateMisfitStacksData();
+                viewModel.updateTheoreticalCurve();
+            }
+        }
+    }
+
+    public void initVesCurvesLineChart(LineChart<Double, Double> vesCurvesLineChart) {
+        this.vesCurvesLineChart = vesCurvesLineChart;
+    }
+
+    public void initModelData(ModelData modelData) {
+        this.modelData = modelData;
+        var points = vesCurvesData.get().get(MOD_CURVE_SERIES_INDEX).getData();
+        for (var point : points) {
+            var resistance = modelData.getResistance();
+            for (int i = 0; i < resistance.size(); i++) {
+                if (point.getYValue() == log10(resistance.get(i))) {
+                    pointResistanceMap.put(point, i);
+                }
             }
         }
     }
