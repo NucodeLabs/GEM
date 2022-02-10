0bf83e7b7898ccf34edc0674afd333388b07f98a
Nikita <iniktus@.mail.ru>
2022-02-10 22:32:54 +0700

    Добавил файлы таблиц. Сейчас допиливать до приемлемого вида буду.

---
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/vestables/MODTable.java
@@ -0,0 +1,23 @@
+package ru.nucodelabs.gem.view.usercontrols.vestables;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import ru.nucodelabs.data.ves.ModelData;
+import ru.nucodelabs.data.ves.Picket;
+import ru.nucodelabs.gem.view.usercontrols.tables.property_data.ModelTableLine;
+import ru.nucodelabs.mvvm.VBUserControl;
+
+public class MODTable extends VBUserControl {
+
+    @FXML
+    TableView<ModelTableLine> modelTable;
+
+    private ModelData data;
+    private ObservableList<ModelTableLine> oModelTableLines;
+
+    public MODTable() {
+        this.modelTable = new TableView<>();
+        this.data = new ModelData();
+    }
+}
---
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/vestables/property_data/VESPropertyData.java
@@ -0,0 +1,6 @@
+package ru.nucodelabs.gem.view.usercontrols.vestables.property_data;
+
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class VESPropertyData {
+}
