bfc89011db28b9b262bf5d509d60c03efb53a488
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-02-17 23:09:14 +0700

    Some improvements in core

--- src/main/java/ru/nucodelabs/gem/core/GemApplication.java
+++ src/main/java/ru/nucodelabs/gem/core/GemApplication.java
@@ -14,7 +14,7 @@ public class GemApplication extends javafx.application.Application {
         ModelFactory modelFactory = new ModelFactory();
         ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
         ResourceBundle uiProperties = ResourceBundle.getBundle("ru/nucodelabs/gem/UI");
-        ViewManager viewManager = new ViewManager(viewModelFactory, stage, uiProperties);
+        ViewManager viewManager = new ViewManager(viewModelFactory, uiProperties);
 
         viewManager.start();
     }
