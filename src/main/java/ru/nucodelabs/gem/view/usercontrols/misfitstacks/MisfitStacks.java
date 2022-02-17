3d12cf60499bf3cee0be77471448c3245421c4ea
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-02-17 14:37:51 +0700

    adjustment

--- src/main/java/ru/nucodelabs/gem/view/usercontrols/misfitstacks/MisfitStacks.java
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/misfitstacks/MisfitStacks.java
@@ -3,23 +3,16 @@ package ru.nucodelabs.gem.view.usercontrols.misfitstacks;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.control.Label;
 import ru.nucodelabs.mvvm.VBUserControl;
 
 public class MisfitStacks extends VBUserControl {
 
-    @FXML
-    private Label label;
     @FXML
     private LineChart<Double, Double> lineChart;
     @FXML
     private NumberAxis lineChartXAxis;
     @FXML
     private NumberAxis lineChartYAxis;
-
-    public Label getLabel() {
-        return label;
-    }
 
     public LineChart<Double, Double> getLineChart() {
         return lineChart;
