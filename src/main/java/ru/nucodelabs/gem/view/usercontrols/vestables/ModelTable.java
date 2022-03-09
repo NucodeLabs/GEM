07cb69a62b6be16d97d491a4038b0a768af52985
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-09 12:04:27 +0700

    Some changes

--- src/main/java/ru/nucodelabs/gem/view/usercontrols/vestables/ModelTable.java
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/vestables/ModelTable.java
@@ -3,6 +3,7 @@ package ru.nucodelabs.gem.view.usercontrols.vestables;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.TableView;
 import ru.nucodelabs.gem.view.usercontrols.VBUserControl;
 import ru.nucodelabs.gem.view.usercontrols.vestables.tablelines.ModelTableLine;
@@ -21,5 +22,17 @@ public class ModelTable extends VBUserControl {
 
     public void setItems(ObservableList<ModelTableLine> value) {
         modelTable.setItems(value);
+    }
+
+    public void setPlaceholder(Node value) {
+        modelTable.setPlaceholder(value);
+    }
+
+    public ObjectProperty<Node> placeholderProperty() {
+        return modelTable.placeholderProperty();
+    }
+
+    public Node getPlaceholder() {
+        return modelTable.getPlaceholder();
     }
 }
