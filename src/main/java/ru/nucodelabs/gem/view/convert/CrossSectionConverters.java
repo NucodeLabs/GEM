610a2979a59e00278517e3722e3d1353cfdc6ca5
Inikta <70467665+Inikta@users.noreply.github.com>
2022-03-19 20:25:38 +0700

    -

--- src/main/java/ru/nucodelabs/gem/view/convert/CrossSectionConverters.java
+++ src/main/java/ru/nucodelabs/gem/view/convert/CrossSectionConverters.java
@@ -8,22 +8,32 @@ import ru.nucodelabs.data.ves.Picket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GeoSectionConverters {
+public class CrossSectionConverters {
 
     public static XYChart.Series<String, Double> getLayerOfPowers(List<Picket> pickets, int layerNum) {
         XYChart.Series<String, Double> layerOfLayerPowers = new XYChart.Series<>();
-        ObservableList<XYChart.Data<String, Double>> obsPowers = new SimpleListProperty<>();
+        //ObservableList<XYChart.Data<String, Double>> obsPowers = new ;
         List<Double> powerList = new ArrayList<>();
+
         for (Picket picket : pickets) {
-            powerList.add(picket.modelData().power().get(layerNum));
+            if (picket.modelData() != null) {
+                powerList.add(picket.modelData().power().get(layerNum));
+            } else {
+                powerList.add(10.0);
+            }
         }
 
-        for (Double layerPower : powerList) {
+        for (Double aDouble : powerList) {
             //XYChart.Data<String, Double> temp = new XYChart.Data<>(((Integer)layerNum).toString(), layerPower);
-            obsPowers.add(new XYChart.Data<>(((Integer)layerNum).toString(), layerPower));
+            String layerNumStr = ((Integer) layerNum).toString();
+            double layerPower = aDouble;
+
+            XYChart.Data<String, Double> pair = new XYChart.Data<>(layerNumStr, layerPower);
+
+            layerOfLayerPowers.getData().add(pair);
         }
 
-        layerOfLayerPowers.setData(obsPowers);
+        //layerOfLayerPowers.setData(obsPowers);
 
         return layerOfLayerPowers;
     }
