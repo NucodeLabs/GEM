27f24de8943a57edb798dd20497828994d103fe5
Michael <plugmihail@gmail.com>
2022-03-22 22:54:51 +0700

    Упростил интерфейс

--- src/main/java/ru/nucodelabs/algorithms/inverse_solver/inverse_functions/InverseFunction.java
+++ src/main/java/ru/nucodelabs/algorithms/inverse_solver/inverse_functions/InverseFunction.java
@@ -3,9 +3,5 @@ package ru.nucodelabs.algorithms.inverse_solver.inverse_functions;
 import java.util.List;
 
 public interface InverseFunction {
-    double functionValue = 0;
-
-    default double getValue(List<Double> solvedResistance, List<Double> experimentalResistance) {
-        return functionValue;
-    }
+    double getValue(List<Double> solvedResistance, List<Double> experimentalResistance);
 }
