bff152017a31b91e5780363003cc2994770fcb63
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-23 15:43:04 +0700

    Стандартный функциональный интерфейс

--- src/main/java/ru/nucodelabs/algorithms/inverse_solver/inverse_functions/InverseFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.nucodelabs.algorithms.inverse_solver.inverse_functions;
-
-import java.util.List;
-
-public interface InverseFunction {
-    double getValue(List<Double> solvedResistance, List<Double> experimentalResistance);
-}
