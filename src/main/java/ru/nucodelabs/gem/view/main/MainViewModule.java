13fca11e7d3418481693af31a4f0bc06e0dbe6c5
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-24 19:35:23 +0700

    Декомпозиция

--- src/main/java/ru/nucodelabs/gem/view/main/MainViewModule.java
+++ src/main/java/ru/nucodelabs/gem/view/main/MainViewModule.java
@@ -3,27 +3,18 @@ package ru.nucodelabs.gem.view.main;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import com.google.inject.TypeLiteral;
 import com.google.inject.name.Named;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableObjectValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.chart.XYChart;
 import ru.nucodelabs.data.ves.Picket;
 import ru.nucodelabs.gem.app.io.FileManager;
 import ru.nucodelabs.gem.view.DialogsModule;
 import ru.nucodelabs.gem.view.FileChoosersModule;
+import ru.nucodelabs.gem.view.SharedObservablesModule;
+import ru.nucodelabs.gem.view.charts.ChartsModule;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static com.google.inject.Scopes.SINGLETON;
-import static ru.nucodelabs.gem.view.charts.VESCurvesController.MOD_CURVE_SERIES_CNT;
 
 /**
  * Зависимости в пределах одного главного окна
@@ -34,13 +25,10 @@ public class MainViewModule extends AbstractModule {
     protected void configure() {
         install(new FileChoosersModule());
         install(new DialogsModule());
+        install(new SharedObservablesModule());
+        install(new ChartsModule());
         bind(MainViewController.class).in(SINGLETON);
         bind(FileManager.class).toProvider(FileManager::createDefaultFileManager);
-        bind(new TypeLiteral<ObjectProperty<Picket>>() {})
-                .to(new TypeLiteral<SimpleObjectProperty<Picket>>() {})
-                .in(SINGLETON);
-        bind(new TypeLiteral<ObservableObjectValue<Picket>>() {})
-                .to(new TypeLiteral<ObjectProperty<Picket>>() {});
     }
 
     @Provides
@@ -59,35 +47,6 @@ public class MainViewModule extends AbstractModule {
     @Named("ImportMOD")
     private Runnable provideImportMOD(MainViewController controller) {
         return controller::importMOD;
-    }
-
-    @Provides
-    private ObjectProperty<ObservableList<XYChart.Series<Double, Double>>> provideChartDataProperty() {
-        return new SimpleObjectProperty<>(
-                FXCollections.observableArrayList(new ArrayList<>()));
-    }
-
-    @Provides
-    @Named("VESCurves")
-    private ObjectProperty<ObservableList<XYChart.Series<Double, Double>>> provideVESCurvesDataProperty() {
-        ObjectProperty<ObservableList<XYChart.Series<Double, Double>>> dataProperty =
-                new SimpleObjectProperty<>(FXCollections.observableArrayList());
-        for (int i = 0; i < MOD_CURVE_SERIES_CNT; i++) {
-            dataProperty.get().add(new XYChart.Series<>());
-        }
-        return dataProperty;
-    }
-
-    @Provides
-    @Singleton
-    private ObservableList<Picket> providePickets() {
-        return FXCollections.observableList(new ArrayList<>());
-    }
-
-    @Provides
-    @Singleton
-    private IntegerProperty provideIntProperty() {
-        return new SimpleIntegerProperty(0);
     }
 
     @Provides
