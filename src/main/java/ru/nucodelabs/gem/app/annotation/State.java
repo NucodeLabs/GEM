5d854977ff7de4e0296ca5eb9aca0c29225af21c
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-03-31 14:40:59 +0700

    @ State аннотация для списка пикетов который является состоянием программы

---
+++ src/main/java/ru/nucodelabs/gem/app/annotation/State.java
@@ -0,0 +1,14 @@
+package ru.nucodelabs.gem.app.annotation;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier
+@Target({FIELD, PARAMETER, METHOD})
+@Retention(RUNTIME)
+public @interface State {
+}
