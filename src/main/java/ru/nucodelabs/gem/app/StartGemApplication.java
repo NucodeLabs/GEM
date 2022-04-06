e00a59de3b35b3a181b34e32fdb7fc0743081694
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-04-06 22:08:53 +0700

    macos костыли из-за отсутствия API

--- src/main/java/ru/nucodelabs/gem/app/StartGemApplication.java
+++ src/main/java/ru/nucodelabs/gem/app/StartGemApplication.java
@@ -1,32 +1,13 @@
 package ru.nucodelabs.gem.app;
 
 import javafx.application.Application;
-import ru.nucodelabs.gem.utils.OSDetect;
-
-import java.awt.*;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Main, запускает приложение JavaFX
  */
 public class StartGemApplication {
 
-    public static final List<String> macOSHandledFiles = new ArrayList<>();
-
     public static void main(String[] args) {
-
-        if (OSDetect.isMacOS()) {
-            Desktop desktop = Desktop.getDesktop();
-            desktop.setOpenFileHandler(e -> {
-                e.getFiles()
-                        .stream()
-                        .map(File::getAbsolutePath)
-                        .forEach(macOSHandledFiles::add);
-            });
-        }
-
         Application.launch(GemApplication.class, args);
     }
 }
