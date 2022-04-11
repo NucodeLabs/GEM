ad59456ecb58eb59a4d611c1d56bea3473ca5dcf
Inikta <70467665+Inikta@users.noreply.github.com>
2022-04-11 09:56:43 +0700

    Теперь тут цвета настраиваемые (пока только через код и немного с багами)

--- src/main/java/ru/nucodelabs/gem/view/cross_section/CrossSectionController.java
+++ src/main/java/ru/nucodelabs/gem/view/cross_section/CrossSectionController.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
@@ -55,9 +56,51 @@ public class CrossSectionController extends AbstractController {
                 FXCollections.observableArrayList(
                         CrossSectionConverters.makeCategories(picketObservableList)));
 
+
         List<XYChart.Series<String, Number>> seriesList = CrossSectionConverters.getLayersOfPower(picketObservableList);
 
         dataProperty.get().setAll(FXCollections.observableArrayList(seriesList));
+        updateBarColor(dataProperty.get());
+
+    }
+
+    private void updateBarColor(List<XYChart.Series<String, Number>> seriesList) {
+        int layer = 0;
+        int picket = 0;
+        for (XYChart.Series<String, Number> series : seriesList) {
+            for (XYChart.Data<String, Number> data : series.getData()) {
+                double resistance = picketObservableList.get(picket).modelData().resistance().get(layer);
+                Node node = data.getNode();
+                //if (node != null) {
+                    if (resistance <= 10) {
+                        node.setStyle("-fx-bar-fill: #74ee6d");
+                    } else if (10 < resistance && resistance <= 30) {
+                        node.setStyle("-fx-bar-fill: #019301");
+                    } else if (30 < resistance && resistance <= 60) {
+                        node.setStyle("-fx-bar-fill: #d0ff00");
+                    } else if (60 < resistance && resistance <= 120) {
+                        node.setStyle("-fx-bar-fill: #f8ff00");
+                    } else if (120 < resistance && resistance <= 200) {
+                        node.setStyle("-fx-bar-fill: #ffd700");
+                    } else if (200 < resistance && resistance <= 300) {
+                        node.setStyle("-fx-bar-fill: #ff9100");
+                    } else if (300 < resistance && resistance <= 500) {
+                        node.setStyle("-fx-bar-fill: #ff6200");
+                    } else if (500 < resistance && resistance <= 1000) {
+                        node.setStyle("-fx-bar-fill: #ff4d00");
+                    } else if (1000 < resistance) {
+                        node.setStyle("-fx-bar-fill: #ff0000");
+                    }
+                    if (resistance == 0) {
+                        node.setStyle("-fx-bar-fill: #ffffff");
+                    }
+                //}
+
+                picket++;
+            }
+            picket %= picketObservableList.size();
+            layer++;
+        }
     }
 
     protected Stage getStage() {
