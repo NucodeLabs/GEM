16b37eb81377c6f03a13b7b98397487cd6ba81bd
Nikita <iniktus@.mail.ru>
2022-05-23 11:44:31 +0700

    Добавил проперти

--- src/main/java/ru/nucodelabs/gem/view/color_palette/ColorPaletteModule.java
+++ src/main/java/ru/nucodelabs/gem/view/color_palette/ColorPaletteModule.java
@@ -2,7 +2,9 @@ package ru.nucodelabs.gem.view.color_palette;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,6 +12,7 @@ import javafx.scene.chart.XYChart;
 import ru.nucodelabs.files.color_palette.CLRData;
 import ru.nucodelabs.files.color_palette.CLRFileParser;
 
+import javax.inject.Named;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -22,5 +25,15 @@ public class ColorPaletteModule extends AbstractModule {
     @Provides ObjectProperty<CLRData> provideCLRPallete() throws Exception {
         return new SimpleObjectProperty<>(
                 new CLRFileParser(new File("../GEM/data/clr/002_ERT_Rainbow_2.clr")).parse());
+    }
+    @Provides
+    @Named
+    DoubleProperty provideMinResistance() {
+        return new SimpleDoubleProperty();
+    }
+
+    @Provides
+    DoubleProperty provideMaxResistance() {
+        return new SimpleDoubleProperty();
     }
 }
