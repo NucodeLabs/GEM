7ebb9f93b1a4c6d21465c762f0c3131590fdd520
Nikita <iniktus@.mail.ru>
2022-05-23 11:44:31 +0700

    Добавил проперти

--- src/main/java/ru/nucodelabs/gem/view/color_palette/ColorPaletteController.java
+++ src/main/java/ru/nucodelabs/gem/view/color_palette/ColorPaletteController.java
@@ -1,10 +1,14 @@
 package ru.nucodelabs.gem.view.color_palette;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import ru.nucodelabs.gem.view.AbstractController;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +17,17 @@ public class ColorPaletteController extends AbstractController {
 
     @FXML
     public Canvas palette;
+    @FXML
+    public TextField minResistanceTF;
+    @FXML
+    public TextField maxResistanceTF;
 
+    @Inject
+    @Named
+    public DoubleProperty minResistanceProperty;
+
+    @Inject
+    public DoubleProperty maxResistanceProperty;
 
     @Override
     protected Stage getStage() {
@@ -22,6 +36,15 @@ public class ColorPaletteController extends AbstractController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    }
 
+    @FXML
+    public void onMinResistanceEdit() {
+        minResistanceProperty.setValue(Integer.parseInt(minResistanceTF.getText()));
+    }
+
+    @FXML
+    public void onMaxResistanceEdit() {
+        maxResistanceProperty.setValue(Integer.parseInt(maxResistanceTF.getText()));
     }
 }
