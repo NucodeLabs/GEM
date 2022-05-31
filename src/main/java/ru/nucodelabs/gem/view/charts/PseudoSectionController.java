13e4f2655e579ba53bb2e97217d3b1a22b686843
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-06-01 01:48:07 +0700

    base

---
+++ src/main/java/ru/nucodelabs/gem/view/charts/PseudoSectionController.java
@@ -0,0 +1,21 @@
+package ru.nucodelabs.gem.view.charts;
+
+import javafx.stage.Stage;
+import ru.nucodelabs.gem.view.AbstractController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PseudoSectionController extends AbstractController {
+
+
+    @Override
+    protected Stage getStage() {
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
