559e0cb8ce876eba2565db88208dc9ea43cc2b86
Vadim <32203382+lilvadim@users.noreply.github.com>
2022-06-01 03:05:49 +0700

    first try

--- src/main/java/ru/nucodelabs/gem/view/charts/PseudoSectionController.java
+++ src/main/java/ru/nucodelabs/gem/view/charts/PseudoSectionController.java
@@ -1,13 +1,25 @@
 package ru.nucodelabs.gem.view.charts;
 
+import javafx.beans.value.ObservableObjectValue;
+import javafx.fxml.FXML;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+import ru.nucodelabs.data.ves.Section;
 import ru.nucodelabs.gem.view.AbstractController;
+import ru.nucodelabs.gem.view.usercontrols.heatmap.HeatMap;
 
+import javax.inject.Inject;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PseudoSectionController extends AbstractController {
 
+    @FXML
+    private HeatMap chart;
+    @Inject
+    private ObservableObjectValue<Section> sectionObservableObjectValue;
 
     @Override
     protected Stage getStage() {
@@ -16,6 +28,24 @@ public class PseudoSectionController extends AbstractController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        sectionObservableObjectValue.addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                update();
+            }
+        });
+    }
 
+    private void update() {
+        var section = sectionObservableObjectValue.get();
+        List<XYChart.Data<Double, Double>> data = new ArrayList<>();
+        for (var picket : section.getPickets()) {
+            for (var expData : picket.getExperimentalData()) {
+                data.add(new XYChart.Data<>(
+                        section.getPickets().indexOf(picket) == 0 ? 0 : section.xOfPicket(picket),
+                        expData.getAb2(),
+                        expData.getResistanceApparent()));
+            }
+        }
+        chart.getData().setAll(data);
     }
 }
