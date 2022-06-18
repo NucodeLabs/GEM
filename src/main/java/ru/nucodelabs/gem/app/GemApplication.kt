eeff11ddb7e4f61ae6fa2329d8f2d2755943d771
lilvadim <vadimmos2002@gmail.com>
2022-06-19 03:04:32 +0700

    code style

--- src/main/java/ru/nucodelabs/gem/app/GemApplication.kt
+++ src/main/java/ru/nucodelabs/gem/app/GemApplication.kt
@@ -10,10 +10,9 @@ import javafx.stage.Stage
 import javafx.stage.Window
 import javafx.stage.WindowEvent
 import javafx.stage.WindowEvent.WINDOW_CLOSE_REQUEST
-import ru.nucodelabs.gem.utils.macOs
+import ru.nucodelabs.gem.utils.OS.macOS
 import ru.nucodelabs.gem.view.main.MainViewController
 import java.io.File
-import java.io.IOException
 import java.util.logging.Logger
 import com.sun.glass.ui.Application as LowLevelApplication
 
@@ -29,21 +28,22 @@ class GemApplication : Application() {
     lateinit var logger: Logger
 
     init {
-        macOs {
+        macOS {
             LowLevelApplication.GetApplication().eventHandler = object : LowLevelApplication.EventHandler() {
                 override fun handleOpenFilesAction(
                     app: com.sun.glass.ui.Application,
                     time: Long,
                     files: Array<String>
                 ) {
-                    macOSHandledFiles.addAll(listOf(*files))
+                    macOSHandledFiles += files
                 }
 
                 override fun handleQuitAction(app: LowLevelApplication, time: Long) {
-                    val winCnt = Window.getWindows().size
-                    for (i in (0 until winCnt).reversed()) {
-                        val window = Window.getWindows()[i]
-                        window.fireEvent(WindowEvent(window, WINDOW_CLOSE_REQUEST))
+                    val windowsLastIdx = Window.getWindows().lastIndex
+                    for (i in windowsLastIdx downTo 0) {
+                        with(Window.getWindows()[i]) {
+                            fireEvent(WindowEvent(this, WINDOW_CLOSE_REQUEST))
+                        }
                     }
                 }
             }
@@ -58,8 +58,7 @@ class GemApplication : Application() {
 
     @Throws(Exception::class)
     override fun start(primaryStage: Stage) {
-        val params: MutableList<String> = parameters.raw.toMutableList()
-        params.addAll(macOSHandledFiles)
+        val params: List<String> = parameters.raw + macOSHandledFiles
         if (params.isNotEmpty()) {
             processParams(params)
         } else {
@@ -78,10 +77,9 @@ class GemApplication : Application() {
         val expFiles = mutableListOf<File>()
         for (param in params) {
             if (param.endsWith(".EXP", ignoreCase = true)) {
-                logger.info("Import EXP, file: $param")
                 expFiles += File(param)
             } else if (param.endsWith("json", ignoreCase = true)) {
-                loadMainViewWithJSONFile(File(param))
+                loadMainViewWithJsonFile(File(param))
             }
         }
         if (expFiles.isNotEmpty()) {
@@ -89,26 +87,21 @@ class GemApplication : Application() {
         }
     }
 
-    private fun loadMainViewWithJSONFile(jsonFile: File) {
-        val fxmlLoader = loadFxmlAndShow()
-        val controller = fxmlLoader.getController<MainViewController>()
-        logger.info("Open JSON Section, file: " + jsonFile.absolutePath)
-        controller.openJsonSection(jsonFile)
-    }
-
-    private fun loadMainViewWithEXPFiles(expFiles: List<File>) {
-        val fxmlLoader = loadFxmlAndShow()
-        val controller = fxmlLoader.getController<MainViewController>()
-        expFiles.forEach { controller.addEXP(it) }
-    }
-
-    private fun loadFxmlAndShow(): FXMLLoader {
-        val fxmlLoader = injector.getInstance(Key.get(FXMLLoader::class.java, Names.named("MainView")))
-        try {
-            fxmlLoader.load<Stage>().show()
-        } catch (e: IOException) {
-            e.printStackTrace()
+    private fun loadMainViewWithJsonFile(jsonFile: File) =
+        fxmlLoaderAfterShow().getController<MainViewController>().run {
+            logger.info("Open JSON Section, file: ${jsonFile.absolutePath}")
+            openJsonSection(jsonFile)
         }
-        return fxmlLoader
-    }
+
+    private fun loadMainViewWithEXPFiles(expFiles: List<File>) =
+        fxmlLoaderAfterShow().getController<MainViewController>().run {
+            expFiles.forEach {
+                logger.info("Import EXP, file: ${it.absolutePath}")
+                addEXP(it)
+            }
+        }
+
+    private fun fxmlLoaderAfterShow(): FXMLLoader = mainViewFxmlLoader().also { it.load<Stage>().show() }
+
+    private fun mainViewFxmlLoader() = injector.getInstance(Key.get(FXMLLoader::class.java, Names.named("MainView")))
 }
