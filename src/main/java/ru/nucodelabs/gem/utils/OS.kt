9dc0ed723926fbe4df0c0556686ede4791270cb1
lilvadim <vadimmos2002@gmail.com>
2022-06-19 03:04:32 +0700

    code style

--- src/main/java/ru/nucodelabs/gem/utils/OS.kt
+++ src/main/java/ru/nucodelabs/gem/utils/OS.kt
@@ -1,9 +1,5 @@
 package ru.nucodelabs.gem.utils
 
-import ru.nucodelabs.gem.utils.OS.isLinuxOrOther
-import ru.nucodelabs.gem.utils.OS.isMacOS
-import ru.nucodelabs.gem.utils.OS.isWindows
-
 object OS {
     private val osNameNormalized = System.getProperty("os.name").lowercase()
 
@@ -15,16 +11,25 @@ object OS {
 
     @JvmStatic
     val isLinuxOrOther = !isMacOS && !isWindows
-}
 
-inline fun macOs(block: () -> Unit) {
-    if (isMacOS) block()
-}
+    /**
+     * `block` runs only on macOS
+     */
+    inline fun macOS(block: () -> Unit) {
+        if (isMacOS) block()
+    }
 
-inline fun linux(block: () -> Unit) {
-    if (isLinuxOrOther) block()
-}
+    /**
+     * `block` runs only on Linux
+     */
+    inline fun linux(block: () -> Unit) {
+        if (isLinuxOrOther) block()
+    }
 
-inline fun windows(block: () -> Unit) {
-    if (isWindows) block()
+    /**
+     * `block` runs only on Windows
+     */
+    inline fun windows(block: () -> Unit) {
+        if (isWindows) block()
+    }
 }
