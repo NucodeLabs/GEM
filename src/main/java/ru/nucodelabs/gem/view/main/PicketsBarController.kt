0d6b063e408b2bd5b0903a3db850a11d70575107
lilvadim <vadimmos2002@gmail.com>
2022-06-19 17:54:10 +0700

    PicketsBarController.kt

--- src/main/java/ru/nucodelabs/gem/view/main/PicketsBarController.kt
+++ src/main/java/ru/nucodelabs/gem/view/main/PicketsBarController.kt
@@ -1,101 +1,93 @@
-package ru.nucodelabs.gem.view.main;
+package ru.nucodelabs.gem.view.main
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.value.ObservableObjectValue;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import ru.nucodelabs.data.ves.Picket;
-import ru.nucodelabs.data.ves.Section;
-import ru.nucodelabs.gem.app.model.SectionManager;
-import ru.nucodelabs.gem.app.snapshot.HistoryManager;
-import ru.nucodelabs.gem.view.AbstractController;
+import javafx.beans.property.IntegerProperty
+import javafx.beans.value.ObservableObjectValue
+import javafx.event.EventHandler
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.ContextMenu
+import javafx.scene.control.MenuItem
+import javafx.scene.layout.HBox
+import javafx.stage.Stage
+import ru.nucodelabs.data.ves.Section
+import ru.nucodelabs.gem.app.model.SectionManager
+import ru.nucodelabs.gem.app.snapshot.HistoryManager
+import ru.nucodelabs.gem.view.AbstractController
+import java.net.URL
+import java.util.*
+import javax.inject.Inject
 
-import javax.inject.Inject;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
-public class PicketsBarController extends AbstractController {
-
-    private final ObservableObjectValue<Section> section;
-    private final IntegerProperty picketIndex;
-
+class PicketsBarController @Inject constructor(
+    private val sectionObservable: ObservableObjectValue<Section>,
+    private val picketIndex: IntegerProperty,
+    private val sectionManager: SectionManager,
+    private val historyManager: HistoryManager<Section>
+) : AbstractController() {
     @FXML
-    private HBox container;
+    private lateinit var container: HBox
 
-    @Inject
-    private SectionManager sectionManager;
-    @Inject
-    private HistoryManager<Section> historyManager;
+    override val stage: Stage?
+        get() = container.scene.window as Stage?
 
-    @Inject
-    public PicketsBarController(
-            ObservableObjectValue<Section> section,
-            IntegerProperty picketIndex) {
+    private val section: Section
+        get() = sectionObservable.get()!!
 
-        this.section = section;
-        this.picketIndex = picketIndex;
-        section.addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                update();
-            }
-        });
-        picketIndex.addListener((observable, oldValue, newValue) -> update());
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    }
-
-    @Override
-    protected Stage getStage() {
-        return (Stage) container.getScene().getWindow();
-    }
-
-    protected void update() {
-        List<Button> buttons = new ArrayList<>();
-        List<Picket> pickets = section.get().getPickets();
-        for (int i = 0; i < pickets.size(); i++) {
-            final int picketNumber = i;
-            Button button = new Button(pickets.get(picketNumber).getName());
-
-            if (i == picketIndex.get()) {
-                button.setStyle(
-                        "-fx-background-color: LightGray;");
-            }
-
-            button.setOnAction(e -> picketIndex.set(picketNumber));
-
-            MenuItem delete = new MenuItem("Удалить"); // TODO использовать UI Properties
-            delete.setOnAction(e -> historyManager.snapshotAfter(() -> sectionManager.remove(picketNumber)));
-
-            MenuItem moveLeft = new MenuItem("Переместить влево");
-            moveLeft.setOnAction(e -> historyManager.snapshotAfter(() -> sectionManager.swap(picketNumber, picketNumber - 1)));
-
-            MenuItem moveRight = new MenuItem("Переместить вправо");
-            moveRight.setOnAction(e -> historyManager.snapshotAfter(() -> sectionManager.swap(picketNumber, picketNumber + 1)));
-
-            if (pickets.size() == 1) {
-                delete.setDisable(true);
-                moveLeft.setDisable(true);
-                moveRight.setDisable(true);
-            }
-            if (picketNumber == 0) {
-                moveLeft.setDisable(true);
-            }
-            if (picketNumber == pickets.size() - 1) {
-                moveRight.setDisable(true);
-            }
-
-            ContextMenu contextMenu = new ContextMenu(delete, moveLeft, moveRight);
-            button.setOnContextMenuRequested(e -> contextMenu.show(getStage(), e.getScreenX(), e.getScreenY()));
-            buttons.add(button);
+    override fun initialize(location: URL, resources: ResourceBundle) {
+        sectionObservable.addListener { _, _, newValue: Section? ->
+            newValue?.let { update() }
         }
-        container.getChildren().setAll(buttons);
+        picketIndex.addListener { _, _, _ -> update() }
+    }
+
+    private fun update() {
+        val buttons = mutableListOf<Button>()
+        val pickets = section.pickets
+
+        for (index in pickets.indices) {
+            // TODO использовать UI Properties
+            val contextMenu = ContextMenu(
+                MenuItem("Удалить").apply {
+                    onAction = EventHandler {
+                        historyManager.snapshotAfter {
+                            sectionManager.remove(index)
+                        }
+                    }
+                    if (pickets.size == 1) {
+                        isDisable = true
+                    }
+                },
+                MenuItem("Переместить влево").apply {
+                    onAction = EventHandler {
+                        historyManager.snapshotAfter {
+                            sectionManager.swap(index, index - 1)
+                        }
+                    }
+                    if (index == 0) {
+                        isDisable = true
+                    }
+                },
+                MenuItem("Переместить вправо").apply {
+                    onAction = EventHandler {
+                        historyManager.snapshotAfter {
+                            sectionManager.swap(index, index + 1)
+                        }
+                    }
+                    if (index == pickets.lastIndex) {
+                        isDisable = true
+                    }
+                }
+            )
+
+            val button = Button(pickets[index].name).apply {
+                if (index == picketIndex.get()) {
+                    style = "-fx-background-color: LightGray;"
+                }
+                onAction = EventHandler { picketIndex.set(index) }
+                onContextMenuRequested = EventHandler { contextMenu.show(stage, it.screenX, it.screenY) }
+            }
+
+            buttons += button
+        }
+        container.children.setAll(buttons)
     }
 }
