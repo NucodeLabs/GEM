d0b092f3aac6102008063c1ecc0ca5e943451713
lilvadim <vadimmos2002@gmail.com>
2022-06-20 01:50:33 +0700

    SonetImportManagerImpl.kt code style

--- src/main/java/ru/nucodelabs/gem/app/io/SonetImportManagerImpl.kt
+++ src/main/java/ru/nucodelabs/gem/app/io/SonetImportManagerImpl.kt
@@ -3,82 +3,74 @@ package ru.nucodelabs.gem.app.io
 import ru.nucodelabs.data.ves.ExperimentalData
 import ru.nucodelabs.data.ves.ModelLayer
 import ru.nucodelabs.data.ves.Picket
-import ru.nucodelabs.files.sonet.EXPFileParser
-import ru.nucodelabs.files.sonet.MODFileParser
-import ru.nucodelabs.files.sonet.STTFile
-import ru.nucodelabs.files.sonet.STTFileParser
+import ru.nucodelabs.files.sonet.*
 import java.io.File
 
 internal class SonetImportManagerImpl : SonetImportManager {
     @Throws(Exception::class)
     override fun loadNameAndExperimentalDataFromEXPFile(expFile: File, target: Picket): Picket {
-        val picketName: String
         val fileName = expFile.name
-        picketName = if (fileName.endsWith(".EXP") || fileName.endsWith(".exp")) {
-            fileName.substring(0, fileName.length - ".EXP".length)
+
+        val picketName = if (fileName.endsWith(".exp", ignoreCase = true)) {
+            fileName.substring(0, fileName.length - ".exp".length)
         } else {
             fileName
         }
+
         return loadExperimentalDataFromEXPFile(expFile, target).withName(picketName)
     }
 
     @Throws(Exception::class)
-    private fun getSTTFile(expFile: File): STTFile {
-        val expFile1 = EXPFileParser(expFile).parse()
+    private fun parseExpWithStt(expFile: File): Pair<EXPFile, STTFile> {
+        val expParsed = EXPFileParser(expFile).parse()
         val expFilePath = expFile.toPath()
-        return STTFileParser(
-            File(
-                expFilePath.parent.toString()
-                        + File.separator
-                        + expFile1.sttFileName
-            )
+        return expParsed to STTFileParser(
+            File("${expFilePath.parent}${File.separator}${expParsed.sttFileName}")
         ).parse()
     }
 
     @Throws(Exception::class)
     override fun loadExperimentalDataFromEXPFile(expFile: File, target: Picket): Picket {
-        val expFile1 = EXPFileParser(expFile).parse()
-        val sttFile = getSTTFile(expFile)
-        val sizes = listOf(
-            sttFile.aB_2,
-            sttFile.mN_2,
-            expFile1.amperage,
-            expFile1.voltage,
-            expFile1.resistanceApparent,
-            expFile1.errorResistanceApparent
-        ).map { it.size }
-        val minSize = sizes.min()
+        val (expParsed, sttParsed) = parseExpWithStt(expFile)
+
+        val minSize = listOf(
+            sttParsed.aB_2,
+            sttParsed.mN_2,
+            expParsed.amperage,
+            expParsed.voltage,
+            expParsed.resistanceApparent,
+            expParsed.errorResistanceApparent
+        ).minOf { it.size }
+
         val expData: MutableList<ExperimentalData> = mutableListOf()
         for (i in 0 until minSize) {
-            expData.add(
-                ExperimentalData.create(
-                    sttFile.aB_2[i],
-                    sttFile.mN_2[i],
-                    expFile1.resistanceApparent[i],
-                    expFile1.errorResistanceApparent[i],
-                    expFile1.amperage[i],
-                    expFile1.voltage[i]
-                )
+            expData += ExperimentalData.create(
+                sttParsed.aB_2[i],
+                sttParsed.mN_2[i],
+                expParsed.resistanceApparent[i],
+                expParsed.errorResistanceApparent[i],
+                expParsed.amperage[i],
+                expParsed.voltage[i]
             )
         }
+
         return target.withExperimentalData(expData)
     }
 
     @Throws(Exception::class)
     override fun loadModelDataFromMODFile(modFile: File, target: Picket): Picket {
-        val modFile1 = MODFileParser(modFile).parse()
-        val sizes = listOf(
-            modFile1.power,
-            modFile1.resistance
-        ).map { it.size }
-        val minSize = sizes.min()
+        val modParsed = MODFileParser(modFile).parse()
+
+        val minSize = listOf(
+            modParsed.power,
+            modParsed.resistance
+        ).minOf { it.size }
+
         val modelData: MutableList<ModelLayer> = mutableListOf()
         for (i in 0 until minSize) {
-            modelData.add(
-                ModelLayer.create(
-                    modFile1.power[i],
-                    modFile1.resistance[i]
-                )
+            modelData += ModelLayer.create(
+                modParsed.power[i],
+                modParsed.resistance[i]
             )
         }
         return target.withModelData(modelData)
