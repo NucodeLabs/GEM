b4c9467e375e113364c92ce0b82cf66ab33fa20a
lilvadim <vadimmos2002@gmail.com>
2022-06-20 17:21:21 +0700

    extensions.fx package

--- src/main/java/ru/nucodelabs/gem/utils/FX.kt
+++
@@ -1,6 +0,0 @@
-package ru.nucodelabs.gem.utils
-
-import javafx.beans.binding.Bindings
-import javafx.collections.ObservableList
-
-fun ObservableList<*>.emptyBinding() = Bindings.createBooleanBinding({ isEmpty() }, this)
