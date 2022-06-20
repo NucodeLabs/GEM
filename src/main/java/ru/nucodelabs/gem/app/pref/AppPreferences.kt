890b3aab8880d72b285ba38a916d80b443eadbd2
lilvadim <vadimmos2002@gmail.com>
2022-06-20 19:39:58 +0700

    Preference.kt improvement

--- src/main/java/ru/nucodelabs/gem/app/pref/AppPreferences.kt
+++ src/main/java/ru/nucodelabs/gem/app/pref/AppPreferences.kt
@@ -3,26 +3,26 @@ package ru.nucodelabs.gem.app.pref
 private val USER_HOME: String = System.getProperty("user.home")
 
 @JvmField
-val RECENT_FILES: Preference<String> = object : Preference<String>("RECENT_FILES", "") {}
+val RECENT_FILES = Preference("RECENT_FILES", "")
 
 @JvmField
-val EXP_FILES_DIR: Preference<String> = object : Preference<String>("EXP_FC_INIT_DIR", USER_HOME) {}
+val EXP_FILES_DIR = Preference("EXP_FC_INIT_DIR", USER_HOME)
 
 @JvmField
-val JSON_FILES_DIR = object : Preference<String>("JSON_FC_INIT_DIR", USER_HOME) {}
+val JSON_FILES_DIR = Preference("JSON_FC_INIT_DIR", USER_HOME)
 
 @JvmField
-val MOD_FILES_DIR = object : Preference<String>("MOD_FC_INIT_DIR", USER_HOME) {}
+val MOD_FILES_DIR = Preference("MOD_FC_INIT_DIR", USER_HOME)
 
 @JvmField
-val MAIN_WINDOW_X = object : Preference<Double>("WINDOW_X", .0) {}
+val MAIN_WINDOW_X = Preference("WINDOW_X", .0)
 
 @JvmField
-val MAIN_WINDOW_Y = object : Preference<Double>("WINDOW_Y", .0) {}
+val MAIN_WINDOW_Y = Preference("WINDOW_Y", .0)
 
 @JvmField
-val MAIN_WINDOW_W = object : Preference<Double>("WINDOW_W", 1280.0) {}
+val MAIN_WINDOW_W = Preference("WINDOW_W", 1280.0)
 
 @JvmField
-val MAIN_WINDOW_H = object : Preference<Double>("WINDOW_H", 720.0) {}
+val MAIN_WINDOW_H = Preference("WINDOW_H", 720.0)
 
