1614a0af6063c5473f932970ea17f66ece35668f
lilvadim <vadimmos2002@gmail.com>
2022-07-06 01:08:09 +0700

    colored

--- src/main/java/ru/nucodelabs/gem/extensions/fx/FX.kt
+++ src/main/java/ru/nucodelabs/gem/extensions/fx/FX.kt
@@ -5,7 +5,10 @@ import javafx.beans.binding.BooleanBinding
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
 import javafx.scene.chart.XYChart
+import javafx.scene.paint.Color
+import java.lang.String.format
 import java.util.*
+import kotlin.math.ceil
 
 typealias Line<X, Y> = XYChart.Series<X, Y>
 
@@ -15,6 +18,14 @@ fun ObservableList<*>.emptyBinding(): BooleanBinding = Bindings.createBooleanBin
 
 operator fun ResourceBundle.get(key: String): String = this.getString(key)
 
-fun <T> List<T>.toObservableList(): ObservableList<T> = FXCollections.observableList(this)
+fun <T> List<T>.toObservableList(): ObservableList<T> = FXCollections.observableList(this.toMutableList())
 
 fun <T> observableListOf(vararg items: T): ObservableList<T> = FXCollections.observableList(mutableListOf(*items))
+
+fun Color.toCss() = format(
+    "rgba(%d, %d, %d, %f)",
+    ceil(red * 255).toInt(),
+    ceil(green * 255).toInt(),
+    ceil(blue * 255).toInt(),
+    opacity
+)
