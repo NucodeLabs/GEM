342c66f7af8cd3db3ba48e9d726443b356729442
lilvadim <vadimmos2002@gmail.com>
2022-07-06 14:05:02 +0700

    heatmap axes

--- src/main/java/ru/nucodelabs/gem/view/usercontrols/heatmap/HeatMap.java
+++ src/main/java/ru/nucodelabs/gem/view/usercontrols/heatmap/HeatMap.java
@@ -2,11 +2,14 @@ package ru.nucodelabs.gem.view.usercontrols.heatmap;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -20,19 +23,30 @@ import java.util.stream.Collectors;
 
 public class HeatMap extends VBUserControl {
 
+    private final StringProperty xAxisLabel = new SimpleStringProperty("X");
+    private final StringProperty yAxisLabel = new SimpleStringProperty("Y");
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private VBox container;
+    @FXML
+    private NumberAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
     private final ObjectProperty<ColorMapper> colorPalette = new SimpleObjectProperty<>();
 
     public HeatMap() {
         canvas.widthProperty().addListener((observable, oldValue, newValue) -> repaint());
         canvas.heightProperty().addListener(((observable, oldValue, newValue) -> repaint()));
 
-        data.get().addListener((ListChangeListener<? super XYChart.Data<Double, Double>>) c -> repaint());
+        data.get().addListener((ListChangeListener<? super XYChart.Data<Double, Double>>) c -> update());
         data.addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 newValue.addListener(
-                        (ListChangeListener<? super XYChart.Data<Double, Double>>) c -> repaint());
+                        (ListChangeListener<? super XYChart.Data<Double, Double>>) c -> update());
             }
-            repaint();
+            update();
         });
 
         colorPalette.addListener((observable, oldValue, newValue) -> {
@@ -42,6 +56,14 @@ public class HeatMap extends VBUserControl {
                 newValue.minValueProperty().addListener((observable1, oldValue1, newValue1) -> repaint());
             }
         });
+
+        xAxis.labelProperty().bind(xAxisLabel);
+        yAxis.labelProperty().bind(yAxisLabel);
+    }
+
+    private void update() {
+        repaint();
+        updateAxes();
     }
 
     private void repaint() {
@@ -65,13 +87,16 @@ public class HeatMap extends VBUserControl {
         }
     }
 
+    private void updateAxes() {
+        xAxis.setLowerBound(data.get().stream().mapToDouble(XYChart.Data::getXValue).min().orElse(0));
+        xAxis.setUpperBound(data.get().stream().mapToDouble(XYChart.Data::getXValue).max().orElse(100));
+
+        yAxis.setLowerBound(data.get().stream().mapToDouble(XYChart.Data::getYValue).min().orElse(0));
+        yAxis.setUpperBound(data.get().stream().mapToDouble(XYChart.Data::getYValue).max().orElse(100));
+    }
+
     private final ObjectProperty<ObservableList<XYChart.Data<Double, Double>>> data
             = new SimpleObjectProperty<>(FXCollections.observableArrayList());
-
-    @FXML
-    private Canvas canvas;
-    @FXML
-    private VBox container;
 
     public ObservableList<XYChart.Data<Double, Double>> getData() {
         return data.get();
@@ -95,5 +120,29 @@ public class HeatMap extends VBUserControl {
 
     public ObjectProperty<ColorMapper> colorPaletteProperty() {
         return colorPalette;
+    }
+
+    public String getXAxisLabel() {
+        return xAxisLabel.get();
+    }
+
+    public void setXAxisLabel(String xAxisLabel) {
+        this.xAxisLabel.set(xAxisLabel);
+    }
+
+    public StringProperty xAxisLabelProperty() {
+        return xAxisLabel;
+    }
+
+    public String getYAxisLabel() {
+        return yAxisLabel.get();
+    }
+
+    public void setYAxisLabel(String yAxisLabel) {
+        this.yAxisLabel.set(yAxisLabel);
+    }
+
+    public StringProperty yAxisLabelProperty() {
+        return yAxisLabel;
     }
 }
