4da8c1147c4f411bc5359559b17824ce08c84f3c
lilvadim <vadimmos2002@gmail.com>
2022-07-07 16:38:42 +0700

    fix test

--- src/test/java/ru/nucodelabs/gem/app/model/SectionManagerTest.java
+++ src/test/java/ru/nucodelabs/gem/app/model/SectionManagerTest.java
@@ -2,6 +2,8 @@ package ru.nucodelabs.gem.app.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.nucodelabs.algorithms.forward_solver.ForwardSolver;
+import ru.nucodelabs.algorithms.inverse_solver.InverseSolver;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,7 +11,7 @@ import java.util.UUID;
 public class SectionManagerTest {
     @Test
     void test() {
-        SectionManager sectionManager = new SectionManager();
+        SectionManager sectionManager = new SectionManager(new InverseSolver(ForwardSolver.createDefault()));
 
         Assertions.assertEquals(Optional.empty(), sectionManager.indexById(UUID.randomUUID()));
     }
