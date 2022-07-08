311349fb9e1208087ee096776de6ca60628a72d2
lilvadim <vadimmos2002@gmail.com>
2022-07-09 01:35:39 +0700

    RectangleChart API

--- src/main/java/ru/nucodelabs/gem/view/charts/RectangleChart.kt
+++ src/main/java/ru/nucodelabs/gem/view/charts/RectangleChart.kt
@@ -2,77 +2,169 @@ package ru.nucodelabs.gem.view.charts
 
 import javafx.beans.NamedArg
 import javafx.beans.binding.Bindings.createDoubleBinding
+import javafx.beans.property.DoubleProperty
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.collections.ListChangeListener
+import javafx.collections.ObservableList
 import javafx.scene.chart.NumberAxis
 import javafx.scene.chart.ScatterChart
-import javafx.scene.shape.Rectangle
 import ru.nucodelabs.gem.extensions.fx.observableListOf
+import javafx.scene.shape.Rectangle as FXRectangle
 
 class RectangleChart(
     @NamedArg("xAxis") private val xAxis: NumberAxis,
     @NamedArg("yAxis") private val yAxis: NumberAxis
 ) : ScatterChart<Number, Number>(xAxis, yAxis) {
 
-    private val _rectangles: MutableList<Rectangle> = mutableListOf()
+    /**
+     * Values for axes
+     */
+    class Rectangle(x: Double, y: Double, width: Double, height: Double, init: Rectangle.() -> Unit = {}) :
+        FXRectangle() {
+        init {
+            this.init()
+        }
 
-    val rectangles: List<Rectangle>
-        get() = _rectangles
+        val xOnAxisProperty: DoubleProperty = SimpleDoubleProperty(x)
+        val yOnAxisProperty: DoubleProperty = SimpleDoubleProperty(y)
+        val widthOnAxisProperty: DoubleProperty = SimpleDoubleProperty(width)
+        val heightOnAxisProperty: DoubleProperty = SimpleDoubleProperty(height)
+
+        var xOnAxis: Double
+            get() = xOnAxisProperty.value
+            set(value) = xOnAxisProperty.set(value)
+
+        var yOnAxis: Double
+            get() = yOnAxisProperty.value
+            set(value) = yOnAxisProperty.set(value)
+
+        var widthOnAxis: Double
+            get() = widthOnAxisProperty.value
+            set(value) = widthOnAxisProperty.set(value)
+
+        var heightOnAxis: Double
+            get() = heightOnAxisProperty.value
+            set(value) = heightOnAxisProperty.set(value)
+    }
+
+    private val _rectangleSeries: MutableMap<Rectangle, Series<Number, Number>> = mutableMapOf()
+    val dataRectangles: ObservableList<Rectangle> = observableListOf()
+
+    init {
+        dataRectangles.addListener(ListChangeListener { change ->
+            if (change.next()) {
+                for (rect in change.list) {
+                    if (rect !in plotChildren) {
+                        setupRectangle(rect)
+                    }
+                }
+
+                for (rect in plotChildren.filterIsInstance<Rectangle>()) {
+                    if (rect !in change.list) {
+                        removeRectangle(rect)
+                    }
+                }
+            }
+        })
+    }
 
     /**
-     * Adds rectangle and its points as series of data. X, Y, width, height properties are bound.
+     * Rectangles and associated series
      */
-    fun addRectangle(x: Double, y: Double, width: Double, height: Double, config: Rectangle.() -> Unit = {}) =
-        Rectangle().apply {
-            this.x = xAxis.getDisplayPosition(x)
-            this.xProperty().bind(
-                createDoubleBinding(
-                    { xAxis.getDisplayPosition(x) },
-                    this@RectangleChart.layoutBoundsProperty(),
-                    this@RectangleChart.boundsInParentProperty(),
-                    this@RectangleChart.boundsInLocalProperty()
-                )
+    val rectangleSeries
+        get() = _rectangleSeries
+
+    private val sizeProperties
+        get() = arrayOf(
+            layoutBoundsProperty(),
+            boundsInParentProperty(),
+            boundsInLocalProperty()
+        )
+
+    private fun setupRectangle(rectangle: Rectangle) {
+        rectangle.x = xAxis.getDisplayPosition(rectangle.xOnAxis)
+        rectangle.xProperty().bind(
+            createDoubleBinding(
+                { xAxis.getDisplayPosition(rectangle.xOnAxis) },
+                rectangle.xOnAxisProperty,
+                *sizeProperties
             )
+        )
 
-            this.y = yAxis.getDisplayPosition(y + height)
-            this.yProperty().bind(
-                createDoubleBinding(
-                    { yAxis.getDisplayPosition(y + height) },
-                    this@RectangleChart.layoutBoundsProperty(),
-                    this@RectangleChart.boundsInParentProperty(),
-                    this@RectangleChart.boundsInLocalProperty()
-                )
+        rectangle.y = yAxis.getDisplayPosition(rectangle.yOnAxis + rectangle.heightOnAxis)
+        rectangle.yProperty().bind(
+            createDoubleBinding(
+                { yAxis.getDisplayPosition(rectangle.yOnAxis + rectangle.heightOnAxis) },
+                rectangle.yOnAxisProperty,
+                rectangle.heightOnAxisProperty,
+                *sizeProperties
             )
+        )
 
-            this.width = xAxis.getDisplayPosition(width)
-            this.widthProperty().bind(
-                createDoubleBinding(
-                    { xAxis.getDisplayPosition(width) },
-                    this@RectangleChart.layoutBoundsProperty(),
-                    this@RectangleChart.boundsInParentProperty(),
-                    this@RectangleChart.boundsInLocalProperty()
-                )
+        rectangle.width = xAxis.getDisplayPosition(rectangle.widthOnAxis)
+        rectangle.widthProperty().bind(
+            createDoubleBinding(
+                { xAxis.getDisplayPosition(rectangle.widthOnAxis) },
+                rectangle.widthOnAxisProperty,
+                *sizeProperties
             )
+        )
 
-            this.height = yAxis.getDisplayPosition(yAxis.upperBound - height)
-            this.heightProperty().bind(
-                createDoubleBinding(
-                    { yAxis.getDisplayPosition(yAxis.upperBound - height) },
-                    this@RectangleChart.layoutBoundsProperty(),
-                    this@RectangleChart.boundsInParentProperty(),
-                    this@RectangleChart.boundsInLocalProperty()
-                )
+        rectangle.height = yAxis.getDisplayPosition(yAxis.upperBound - rectangle.heightOnAxis)
+        rectangle.heightProperty().bind(
+            createDoubleBinding(
+                { yAxis.getDisplayPosition(yAxis.upperBound - rectangle.heightOnAxis) },
+                rectangle.heightOnAxisProperty,
+                yAxis.upperBoundProperty(),
+                *sizeProperties
             )
+        )
 
-            _rectangles += this
-
-            data += Series(
-                observableListOf(
-                    Data(x, y),
-                    Data(x + width, y),
-                    Data(x, y + height),
-                    Data(x + width, y + height)
-                )
+        val series = Series(
+            observableListOf(
+                Data(
+                    rectangle.xOnAxis as Number,
+                    rectangle.yOnAxis as Number
+                ).also {
+                    it.XValueProperty().bind(rectangle.xOnAxisProperty.asObject())
+                    it.YValueProperty().bind(rectangle.yOnAxisProperty.asObject())
+                },
+                Data(
+                    (rectangle.xOnAxis + rectangle.widthOnAxis) as Number,
+                    rectangle.yOnAxis as Number
+                ).also {
+                    it.XValueProperty().bind(rectangle.xOnAxisProperty.add(rectangle.widthOnAxisProperty).asObject())
+                    it.YValueProperty().bind(rectangle.yOnAxisProperty.asObject())
+                },
+                Data(
+                    rectangle.xOnAxis as Number,
+                    (rectangle.yOnAxis + rectangle.heightOnAxis) as Number
+                ).also {
+                    it.XValueProperty().bind(rectangle.xOnAxisProperty.asObject())
+                    it.YValueProperty().bind(rectangle.yOnAxisProperty.add(rectangle.heightOnAxisProperty).asObject())
+                },
+                Data(
+                    (rectangle.xOnAxis + rectangle.widthOnAxis) as Number,
+                    (rectangle.yOnAxis + rectangle.heightOnAxis) as Number
+                ).also {
+                    it.XValueProperty().bind(rectangle.xOnAxisProperty.add(rectangle.widthOnAxisProperty).asObject())
+                    it.YValueProperty().bind(rectangle.yOnAxisProperty.add(rectangle.heightOnAxisProperty).asObject())
+                }
             )
+        )
 
-            plotChildren += this
-        }.also { it.config() }
+        data += series
+
+        _rectangleSeries[rectangle] = series
+
+        plotChildren += rectangle
+
+        rectangle.viewOrder = series.data.maxOf { it.node.viewOrder } + 1
+    }
+
+    private fun removeRectangle(rectangle: Rectangle) {
+        plotChildren.remove(rectangle)
+        data.removeAll(_rectangleSeries[rectangle])
+        _rectangleSeries.remove(rectangle)
+    }
 }
--- src/test/java/ru/nucodelabs/gem/view/charts/RectangleChartTest.kt
+++ src/test/java/ru/nucodelabs/gem/view/charts/RectangleChartTest.kt
@@ -1,31 +1,97 @@
 package ru.nucodelabs.gem.view.charts
 
 import javafx.application.Application
+import javafx.application.Platform
+import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.chart.NumberAxis
+import javafx.scene.control.Button
+import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import javafx.stage.Stage
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import ru.nucodelabs.gem.view.charts.RectangleChart.Rectangle
 
 internal class RectangleChartTest {
 
+    @Test
+    @Disabled
+    fun launchApp() {
+        Application.launch(TestApp::class.java)
+    }
+
     class TestApp : Application() {
+        private val chart = RectangleChart(
+            NumberAxis(0.0, 100.0, 1.0),
+            NumberAxis(0.0, 100.0, 1.0)
+        )
+
         override fun start(primaryStage: Stage) {
-            val chart = RectangleChart(
-                NumberAxis(0.0, 100.0, 1.0),
-                NumberAxis(0.0, 100.0, 1.0)
-            )
-            primaryStage.scene = Scene(chart)
+            chart.dataRectangles += Rectangle(25.0, 30.0, 10.0, 20.0) { fill = Color.BLACK }
+            chart.dataRectangles += Rectangle(80.0, 90.0, 50.0, 50.0) { fill = Color.RED }
+            val yellow = Rectangle(-50.0, -40.0, 60.0, 50.0) { fill = Color.YELLOW }
+            chart.dataRectangles += yellow
+
+            val testBtn1 = Button("Change X/Y values for axes").apply {
+                onAction = EventHandler {
+                    val rect = chart.dataRectangles[1]
+                    rect.xOnAxis -= 10
+                    rect.yOnAxis -= 10
+                }
+            }
+
+            val testBtn2 = Button("Change W/H values for axes").apply {
+                onAction = EventHandler {
+                    val rect = chart.dataRectangles[1]
+                    rect.widthOnAxis -= 10
+                    rect.heightOnAxis -= 10
+                }
+            }
+
+            primaryStage.scene = Scene(VBox(chart, testBtn1, testBtn2))
             primaryStage.show()
 
-            chart.addRectangle(25.0, 30.0, 10.0, 20.0) { fill = Color.BLACK }
-            chart.addRectangle(80.0, 90.0, 50.0, 50.0) { fill = Color.RED }
-            chart.addRectangle(-50.0, -40.0, 60.0, 50.0) { fill = Color.YELLOW }
+            chart.dataRectangles.remove(yellow)
         }
     }
 
+    private val chart = RectangleChart(
+        NumberAxis(0.0, 100.0, 1.0),
+        NumberAxis(0.0, 100.0, 1.0)
+    )
+
+    companion object Init {
+        @BeforeAll
+        @JvmStatic
+        fun initJfxRuntime() {
+            Platform.startup { }
+        }
+    }
+
+
     @Test
     fun addRectangle() {
-        Application.launch(TestApp::class.java)
+        chart.dataRectangles += Rectangle(25.0, 30.0, 10.0, 20.0) { fill = Color.BLACK }
+        chart.dataRectangles += Rectangle(80.0, 90.0, 50.0, 50.0) { fill = Color.RED }
+        chart.dataRectangles += Rectangle(-50.0, -40.0, 60.0, 50.0) { fill = Color.YELLOW }
+        chart.dataRectangles += Rectangle(-50.0, -40.0, 60.0, 50.0) { fill = Color.YELLOW }
+
+        assertEquals(4, chart.rectangleSeries.size)
+        assertEquals(4, chart.dataRectangles.size)
+        assertEquals(4, chart.data.size)
+    }
+
+    @Test
+    fun removeRectangle() {
+        val addedRect = Rectangle(0.0, 0.0, 10.0, 10.0)
+        chart.dataRectangles += addedRect
+        chart.dataRectangles.remove(addedRect)
+
+        assertTrue(chart.rectangleSeries.isEmpty())
+        assertTrue(chart.data.isEmpty())
     }
 }
