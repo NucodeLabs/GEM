90207dc3f72e9b1856b392d6e747568dd1d3f26a
SiegKlass <plugmihail@gmail.com>
2022-07-12 21:56:46 +0700

    Забыл чекаут

--- src/main/java/ru/nucodelabs/algorithms/interpolation/Interpolator.kt
+++ src/main/java/ru/nucodelabs/algorithms/interpolation/Interpolator.kt
@@ -6,6 +6,13 @@ class Interpolator (
     private val inputData: List<List<XYChart.Data<Double, Double>>>,
     private var interpolationParser: InterpolationDataParser = InterpolationParser(inputData)
 ) {
+    private lateinit var grid: List<List<XYChart.Data<Double, Double>>>
+    private lateinit var missedPoints: List<List<XYChart.Data<Double, Double>>>
+    private fun initGrid() {
+        interpolationParser.parse()
+        grid = interpolationParser.getGrid()
+        missedPoints = interpolationParser.getMissedPoints()
+    }
 
     private fun adjustGrid() {
 
