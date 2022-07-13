4ce520dd177afc4323646094c5da51e47dd83982
SiegKlass <plugmihail@gmail.com>
2022-07-13 11:22:55 +0700

    Почти готовая интерполяция

--- src/main/java/ru/nucodelabs/algorithms/interpolation/ApacheInterpolator1D.kt
+++ src/main/java/ru/nucodelabs/algorithms/interpolation/ApacheInterpolator1D.kt
@@ -3,7 +3,7 @@ package ru.nucodelabs.algorithms.interpolation
 import org.apache.commons.math3.analysis.interpolation.SplineInterpolator
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction
 
-class ApacheInterpolator1D(): Interpolator1D {
+class ApacheInterpolator1D: Interpolator1D {
     override fun interpolate(x: DoubleArray, f: DoubleArray): PolynomialSplineFunction {
         val splineInterpolator = SplineInterpolator()
         return splineInterpolator.interpolate(x, f)
