b04457f0d0088d23be30648e227cc7d7802e45d7
lilvadim <vadimmos2002@gmail.com>
2022-07-13 12:52:36 +0700

    fixes

--- src/main/java/ru/nucodelabs/gem/view/control/chart/NucodeNumberAxis.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/NucodeNumberAxis.kt
@@ -1,19 +1,27 @@
 package ru.nucodelabs.gem.view.control.chart
 
+import javafx.beans.InvalidationListener
 import javafx.beans.NamedArg
 import javafx.beans.property.SimpleDoubleProperty
+import ru.nucodelabs.gem.extensions.fx.childrenTextNodes
 import kotlin.math.ceil
 import kotlin.math.floor
-import kotlin.math.round
 
 class NucodeNumberAxis @JvmOverloads constructor(
     @NamedArg("lowerBound") lowerBound: Double = 0.0,
     @NamedArg("upperBound") upperBound: Double = 100.0
 ) : InvertibleValueAxis<Number>(lowerBound, upperBound) {
 
-    init {
-        // bugfix: autoRanging is true by default, but it won't work until you set it manually
-        isAutoRanging = true
+    override fun layoutChildren() {
+        super.layoutChildren()
+        tickMarks.forEach { it.isTextVisible = true }
+        childrenTextNodes.forEachIndexed { i, text -> text.isVisible = tickMarks.getOrNull(i)?.isTextVisible ?: false }
+
+        scale = calculateNewScale(
+            if (side.isHorizontal) width else height,
+            lowerBound,
+            upperBound
+        )
     }
 
     private val _tickUnit = SimpleDoubleProperty(10.0)
@@ -22,21 +30,22 @@ class NucodeNumberAxis @JvmOverloads constructor(
         get() = _tickUnit.get()
         set(value) = _tickUnit.set(value)
 
+    init {
+        tickUnitProperty().addListener(InvalidationListener {
+            if (!isAutoRanging) {
+                invalidateRange()
+                requestAxisLayout()
+            }
+        })
+    }
+
     override fun getDisplayPosition(value: Number): Double {
-        val interval = upperBound - lowerBound
         val intervalToVal = value.toDouble() - lowerBound
-        val proportion = intervalToVal / interval
+        val res = intervalToVal * scale
+        // scale is negative if vertical axis
         return when {
-            side.isVertical -> if (!inverted) {
-                height - height * proportion
-            } else {
-                height * proportion
-            }
-            side.isHorizontal -> if (!inverted) {
-                width * proportion
-            } else {
-                width - width * proportion
-            }
+            side.isVertical -> if (!inverted) height + res else -res
+            side.isHorizontal -> if (!inverted) res else width - res
             else -> throw IllegalStateException("что-то пошло не так")
         }
     }
@@ -83,15 +92,8 @@ class NucodeNumberAxis @JvmOverloads constructor(
                     )
                 } else {
                     if (lowerBound + tickUnit < upperBound) {
-                        // If tickUnit is integer, start with the nearest integer
-                        var major = if (round(tickUnit) == tickUnit) {
-                            ceil(lowerBound)
-                        } else {
-                            lowerBound + tickUnit
-                        }
-
+                        var major = if (tickUnit % 1 == 0.0) lowerBound + tickUnit else ceil(lowerBound)
                         val count = ceil((upperBound - major) / tickUnit).toInt()
-
                         for (i in 1..count) {
                             if (major >= upperBound) {
                                 break
@@ -129,7 +131,7 @@ class NucodeNumberAxis @JvmOverloads constructor(
                 )
                 return minorTicks
             }
-            val tickUnitIsInteger = round(tickUnit) == tickUnit
+            val tickUnitIsInteger = tickUnit % 1 == 0.0
             if (tickUnitIsInteger) {
                 var minor = floor(lowerBound) + minorUnit
                 val count = ceil((ceil(lowerBound) - minor) / minorUnit).toInt()
@@ -159,7 +161,7 @@ class NucodeNumberAxis @JvmOverloads constructor(
                 i++
             }
         }
-        return minorTicks
+        return if (!inverted) minorTicks else minorTicks.reversed()
     }
 
     override fun getTickMarkLabel(value: Number?): String = tickLabelFormatter?.toString(value) ?: value.toString()
