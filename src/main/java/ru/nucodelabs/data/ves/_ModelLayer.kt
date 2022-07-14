55fc42ef685f5e19044e3b1e13cc39be6b596899
lilvadim <vadimmos2002@gmail.com>
2022-07-14 22:40:55 +0700

    effectiveExperimentalData

--- src/main/java/ru/nucodelabs/data/ves/_ModelLayer.kt
+++
@@ -1,5 +0,0 @@
-package ru.nucodelabs.data.ves
-
-//fun ru.nucodelabs.data.ves.ModelLayer.toKt() = ModelLayer(power, resistance, isFixedPower, isFixedResistance)
-
-//fun ModelLayer.toOld() = ru.nucodelabs.data.ves.ModelLayer.create(power, resistance, isPowerFixed, isResistanceFixed)
