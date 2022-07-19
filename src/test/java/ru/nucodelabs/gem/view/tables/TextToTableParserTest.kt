0ce87ad6544c24d4e8095fa118a089348e640278
vadim <vadimos2002@gmail.com>
2022-07-19 18:27:40 +0700

    поддержка вставки из буффера

--- src/test/java/ru/nucodelabs/gem/view/tables/TextToTableParserTest.kt
+++ src/test/java/ru/nucodelabs/gem/view/tables/TextToTableParserTest.kt
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test
 internal class TextToTableParserTest {
     @Test
     fun parse() {
-        val testString = "123.3\t324234.5\t123124     \n124234\t32434\t434343\n12.3\t0.1"
+        val testString = "123\t12\t123\n" +
+                "3212\t4512\t43"
 
-        val result = TextToTableParser(testString).parsedTable
+        val textToTableParser = TextToTableParser(testString)
+        val result = textToTableParser.parsedTable
     }
 }
