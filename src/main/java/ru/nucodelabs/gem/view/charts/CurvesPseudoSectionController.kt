17084e0158b8a04cb1e700bfe6936deb8af15da8
vadim <vadimos2002@gmail.com>
2022-07-20 13:14:36 +0700

    update

--- src/main/java/ru/nucodelabs/gem/view/charts/CurvesPseudoSectionController.kt
+++ src/main/java/ru/nucodelabs/gem/view/charts/CurvesPseudoSectionController.kt
@@ -2,8 +2,11 @@ package ru.nucodelabs.gem.view.charts
 
 import javafx.fxml.FXML
 import javafx.scene.chart.LineChart
+import javafx.scene.chart.XYChart
 import javafx.util.StringConverter
+import ru.nucodelabs.algorithms.charts.curves_chart.CurvesChartParser
 import ru.nucodelabs.data.fx.ObservableSection
+import ru.nucodelabs.gem.extensions.fx.toObservableList
 import javax.inject.Inject
 
 class CurvesPseudoSectionController @Inject constructor(
@@ -14,6 +17,14 @@ class CurvesPseudoSectionController @Inject constructor(
     private lateinit var chart: LineChart<Number, Number>
 
     override fun update() {
-
+        chart.data.clear()
+        chart.data += CurvesChartParser(observableSection.asSection()).getPoints().map { curve ->
+            XYChart.Series(curve.map { p ->
+                XYChart.Data(
+                    p.x as Number,
+                    p.y as Number
+                )
+            }.toObservableList())
+        }
     }
 }
