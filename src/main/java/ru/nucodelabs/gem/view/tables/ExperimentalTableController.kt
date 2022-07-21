98117898aa91f49547031b3dc628c418695c721f
vadim <vadimos2002@gmail.com>
2022-07-21 12:51:57 +0700

    пропускает полупустые, выдает номер ошибочной строки

--- src/main/java/ru/nucodelabs/gem/view/tables/ExperimentalTableController.kt
+++ src/main/java/ru/nucodelabs/gem/view/tables/ExperimentalTableController.kt
@@ -303,14 +303,15 @@ class ExperimentalTableController @Inject constructor(
         val parser = TextToTableParser(Clipboard.getSystemClipboard().string)
         try {
             val a = "abcdefghijklmnopqrstuvwxyz".uppercase().toCharArray()
+            val parsedTable = parser.parsedTable.filter { row -> row.none { it == null } }
             val pastedItems: List<ExperimentalData> = when (parser.columnsCount) {
-                3 -> parser.parsedTable.mapIndexed { i, row ->
+                3 -> parsedTable.mapIndexed { i, row ->
                     val ab2 = row[0]?.replace(',', '.')?.toDoubleOrNullBy(decimalFormat)
                         ?: throw IllegalStateException("${a[0]}${i + 1} - Ожидалось AB/2, было ${row[0]}")
                     val mn2 = row[1]?.replace(',', '.')?.toDoubleOrNullBy(decimalFormat)
                         ?: throw IllegalStateException("${a[1]}${i + 1} - Ожидалось MN/2, было ${row[1]}")
                     val resApp = row[2]?.replace(',', '.')?.toDoubleOrNullBy(decimalFormat)
-                        ?: throw IllegalStateException("${a[2]}${i + 1}} - Ожидалось ρₐ, было ${row[2]}")
+                        ?: throw IllegalStateException("${a[2]}${i + 1} - Ожидалось ρₐ, было ${row[2]}")
                     val amp = 100.0
                     val volt = u(resApp, 100.0, k(ab2, mn2))
                     ExperimentalData(
@@ -321,7 +322,7 @@ class ExperimentalTableController @Inject constructor(
                         voltage = volt
                     )
                 }
-                4 -> parser.parsedTable.mapIndexed { i, row ->
+                4 -> parsedTable.mapIndexed { i, row ->
                     ExperimentalData(
                         ab2 = row[0]?.replace(',', '.')?.toDoubleOrNullBy(decimalFormat)
                             ?: throw IllegalStateException("${a[0]}${i + 1} - Ожидалось AB/2, было ${row[0]}"),
@@ -333,7 +334,7 @@ class ExperimentalTableController @Inject constructor(
                             ?: throw IllegalStateException("${a[3]}${i + 1} - Ожидалось I, было ${row[3]}")
                     )
                 }
-                5 -> parser.parsedTable.mapIndexed { i, row ->
+                5 -> parsedTable.mapIndexed { i, row ->
                     ExperimentalData(
                         ab2 = row[0]?.replace(',', '.')?.toDoubleOrNullBy(decimalFormat)
                             ?: throw IllegalStateException("${a[0]}${i + 1} - Ожидалось AB/2, было ${row[0]}"),
@@ -347,8 +348,13 @@ class ExperimentalTableController @Inject constructor(
                             ?: throw IllegalStateException("${a[4]}${i + 1} - Ожидалось ρₐ, было ${row[4]}")
                     )
                 }
-                else -> throw IllegalStateException(
-                    """
+                else -> {
+                    val errorRow = parsedTable.find { row -> row.count { it != null } !in arrayOf(3, 4, 5) }
+                    val errorRowIdx = parsedTable.indexOf(errorRow)
+                    throw IllegalStateException(
+                        """
+                    Строка ${errorRowIdx + 1}: ${errorRow?.asList()}
+                    
                     Допустимые числа колонок: 3, 4, 5.
                     Соответствующий порядок:
                     3 колонки - AB/2, MN/2 и ρₐ
@@ -361,7 +367,8 @@ class ExperimentalTableController @Inject constructor(
                     
                     Погрешность берется δρₐ = 5%
                     """.trimIndent()
-                )
+                    )
+                }
             }
             for (item in pastedItems) {
                 val violations = validator.validate(item)
