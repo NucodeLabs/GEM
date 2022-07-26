08597c71f386ee13f7d14c461859a99550c1c86f
SiegKlass <plugmihail@gmail.com>
2022-07-26 15:55:55 +0700

    Limited drugged zoomed

--- src/main/java/ru/nucodelabs/gem/view/control/chart/ZoomAxis.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/ZoomAxis.kt
@@ -3,20 +3,40 @@ package ru.nucodelabs.gem.view.control.chart
 import javafx.scene.chart.ValueAxis
 import ru.nucodelabs.gem.extensions.std.exp10
 import kotlin.math.log10
+import kotlin.math.max
+import kotlin.math.min
 
 class ZoomAxis(
     val xAxis: ValueAxis<Number>,
     val yAxis: ValueAxis<Number>
 ) {
     fun zoom(scale: Double, position: Pair<Double, Double> = Pair(0.5, 0.5)) {
+        if (scale < 0 || position.first < 0 || position.first > 1 || position.second < 0 || position.second > 1)
+            return
+
+        val minRange = min(xAxis.upperBound - xAxis.lowerBound, yAxis.upperBound - yAxis.lowerBound)
+        val maxRange = max(xAxis.upperBound - xAxis.lowerBound, yAxis.upperBound - yAxis.lowerBound)
+        if ((minRange < 1.0 && scale > 1.0) || (maxRange > 100_000_000 && scale < 1.0))
+            return
+
         zoomAxis(xAxis, scale, position.first)
         zoomAxis(yAxis, scale, position.second)
     }
 
-    private fun zoomAxis(axis: ValueAxis<Number>, scale: Double, position: Double) {
-        if (scale < 0 || position < 0 || position > 1)
-            throw Exception("zoomAxis wrong data")
+    fun drug(deltaCoords: Pair<Double, Double>) {
+        dragAxis(xAxis, deltaCoords.first)
+        dragAxis(yAxis, deltaCoords.second)
+    }
 
+    private fun dragAxis(axis: ValueAxis<Number>, deltaPos: Double) {
+        val lowerBound = axis.lowerBound
+        val upperBound = axis.upperBound
+
+        axis.lowerBound = exp10(((log10(upperBound) - log10(lowerBound)) * deltaPos) + log10(lowerBound))
+        axis.upperBound = exp10(((log10(upperBound) - log10(lowerBound)) * deltaPos) + log10(upperBound))
+    }
+
+    private fun zoomAxis(axis: ValueAxis<Number>, scale: Double, position: Double) {
         val lowerBound = axis.lowerBound
         val upperBound = axis.upperBound
 
