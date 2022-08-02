13664f2a8ecd90df9fbda7efd8732d3b9920d531
lilvadim <vadimmos2002@gmail.com>
2022-08-02 16:07:38 +0700

    поправил дефолтные значения

--- src/main/java/ru/nucodelabs/gem/app/pref/UIPreferences.kt
+++ src/main/java/ru/nucodelabs/gem/app/pref/UIPreferences.kt
@@ -7,10 +7,10 @@ val VES_CURVES_LEGEND_VISIBLE = Preference("VES_CURVES_LEGEND", false)
 val GRAPHS_TITLES = Preference("GRAPHS_TITLES", true)
 
 @JvmField
-val COLOR_MIN_VALUE = Preference("COLOR_MIN_VALUE", 0.1)
+val COLOR_MIN_VALUE = Preference("COLOR_MIN_VALUE", 1.0)
 
 @JvmField
-val COLOR_MAX_VALUE = Preference("COLOR_MAX_VALUE", 100_000.0)
+val COLOR_MAX_VALUE = Preference("COLOR_MAX_VALUE", 10_000.0)
 
 @JvmField
 val COLOR_SEGMENTS = Preference("COLOR_SEGMENTS", 15)
