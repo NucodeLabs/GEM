fb5ca4b3e8fdae32723310f0db394cd74252cc17
lilvadim <vadimmos2002@gmail.com>
2022-08-04 17:13:32 +0700

    Reset Layout

--- src/main/java/ru/nucodelabs/gem/app/pref/UIPreferences.kt
+++ src/main/java/ru/nucodelabs/gem/app/pref/UIPreferences.kt
@@ -1,3 +1,4 @@
+@file:JvmName("UIPreferences")
 package ru.nucodelabs.gem.app.pref
 
 @JvmField
@@ -14,3 +15,17 @@ val COLOR_MAX_VALUE = Preference("COLOR_MAX_VALUE", 10_000.0)
 
 @JvmField
 val COLOR_SEGMENTS = Preference("COLOR_SEGMENTS", 15)
+
+// MainSplitLayoutView
+
+@JvmField
+val VES_SECTION_SPLIT_DIV = Preference("VES_SECT_SPLIT_DIV", 0.5)
+
+@JvmField
+val SECTION_SPLIT_DIV = Preference("SECT_SPLIT_DIV", 0.5)
+
+@JvmField
+val VES_MISFIT_SPLIT_DIV = Preference("VES_MSF_SPLIT_DIV", 0.3)
+
+@JvmField
+val CURVES_TABLE_SPLIT_DIV = Preference("CURVES_TABLE_SPLIT_DIV", 0.6)
