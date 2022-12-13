f834e9b5cec146df3f754fba0e6907c7012097d0
lilvadim <vadimmos2002@gmail.com>
2022-12-14 03:51:30 +0700

    docs + some props

--- src/main/java/ru/nucodelabs/geo/map/MapSizer.kt
+++ src/main/java/ru/nucodelabs/geo/map/MapSizer.kt
@@ -12,23 +12,52 @@ class MapSizer(
     private val maxAb2WithAzimuth: Collection<AzimuthPoint>,
     val scale: Double
 ) {
+    /**
+     * Maximal absolute distance by X from center that points take in meters
+     */
     val maxAbsXFromCenterInMeters: Double by lazy {
         maxAb2WithAzimuth.maxOf { abs(xFromCenter(it.distFromCenterInMeters, it.azimuthInDegrees)) }
     }
 
+    /**
+     * Maximal absolute distance by Y from center that points take in meters
+     */
     val maxAbsYFromCenterInMeters: Double by lazy {
         maxAb2WithAzimuth.maxOf { abs(yFromCenter(it.distFromCenterInMeters, it.azimuthInDegrees)) }
     }
 
+    /**
+     * Maximal absolute distance by Y from center that points take in meters with scale applied
+     */
+    val maxAbsYFromCenterScaledInMeters: Double by lazy {
+        maxAbsYFromCenterInMeters * coefficient
+    }
+
+    /**
+     * Maximal absolute distance by X from center that points take in meters with scale applied
+     */
+    val maxAbsXFromCenterScaledInMeters: Double by lazy {
+        maxAbsXFromCenterInMeters * coefficient
+    }
+
     private val coefficient = 1.0 / scale
 
+    /**
+     * Calculated bottom left corner of rectangle in WSG
+     */
     val bottomLeftCorner: Wsg by lazy {
         center + Dx(-maxAbsXFromCenterInMeters * coefficient) + Dy(-maxAbsYFromCenterInMeters * coefficient)
     }
 
+    /**
+     * Calculated upper right corner of rectangle in WSG
+     */
     val upperRightCorner: Wsg by lazy {
         center + Dx(maxAbsXFromCenterInMeters * coefficient) + Dy(maxAbsYFromCenterInMeters * coefficient)
     }
 }
 
+/**
+ * Stores point coordinates using distance from center and azimuthal angle
+ */
 data class AzimuthPoint(val distFromCenterInMeters: Double, val azimuthInDegrees: Double)
