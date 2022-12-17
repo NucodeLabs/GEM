c93636e98f027d3019210b8fae3dfe4467dbe335
lilvadim <vadimmos2002@gmail.com>
2022-12-17 22:03:52 +0700

    update

--- src/main/java/ru/nucodelabs/gem/view/charts/AnisotropyMapImageProvider.kt
+++ src/main/java/ru/nucodelabs/gem/view/charts/AnisotropyMapImageProvider.kt
@@ -4,17 +4,17 @@ import javafx.scene.image.Image
 import ru.nucodelabs.gem.net.MapImageProvider
 import ru.nucodelabs.geo.map.AzimuthPoint
 import ru.nucodelabs.geo.map.MapSizer
-import ru.nucodelabs.geo.map.Wsg
+import ru.nucodelabs.geo.map.WGS
 import javax.inject.Inject
 
 class AnisotropyMapImageProvider @Inject constructor(
-    private val mapImageProvider: MapImageProvider
+    val mapImageProvider: MapImageProvider
 ) {
     /**
      * Returns satellite image
      * @throws ru.nucodelabs.gem.net.WrongResponseException if API call response is not image
      */
-    fun satImage(center: Wsg, maxAb2WithAzimuth: Collection<AzimuthPoint>, scale: Double = 0.8): Image {
+    fun satImage(center: WGS, maxAb2WithAzimuth: Collection<AzimuthPoint>, scale: Double = 0.8): Image {
         val mapSizer = MapSizer(center, maxAb2WithAzimuth, scale)
         val bottomLeft = mapSizer.bottomLeftCorner
         val upperRight = mapSizer.upperRightCorner
