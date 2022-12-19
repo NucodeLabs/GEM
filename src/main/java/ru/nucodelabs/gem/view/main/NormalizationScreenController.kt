bb4481b460790aa4bcf2a298c2045abeb4bfa232
lilvadim <vadimmos2002@gmail.com>
2022-12-19 11:31:19 +0700

    fix

--- src/main/java/ru/nucodelabs/gem/view/main/NormalizationScreenController.kt
+++ src/main/java/ru/nucodelabs/gem/view/main/NormalizationScreenController.kt
@@ -1,10 +1,7 @@
 package ru.nucodelabs.gem.view.main
 
 import javafx.beans.binding.Bindings.createStringBinding
-import javafx.beans.property.BooleanProperty
-import javafx.beans.property.DoubleProperty
-import javafx.beans.property.SimpleBooleanProperty
-import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.*
 import javafx.beans.value.ObservableObjectValue
 import javafx.collections.ListChangeListener
 import javafx.event.EventHandler
@@ -12,20 +9,25 @@ import javafx.fxml.FXML
 import javafx.scene.chart.LineChart
 import javafx.scene.chart.XYChart
 import javafx.scene.chart.XYChart.Series
-import javafx.scene.control.TableColumn
-import javafx.scene.control.TableView
-import javafx.scene.control.cell.CheckBoxTableCell
+import javafx.scene.control.*
+import javafx.scene.control.cell.TextFieldTableCell
 import javafx.stage.Stage
 import javafx.util.Callback
+import javafx.util.StringConverter
 import ru.nucodelabs.algorithms.normalization.FixableValue
 import ru.nucodelabs.algorithms.normalization.distinctMn2
 import ru.nucodelabs.algorithms.normalization.normalizeExperimentalData
+import ru.nucodelabs.gem.app.snapshot.HistoryManager
 import ru.nucodelabs.gem.fxmodel.ObservableSection
-import ru.nucodelabs.geo.ves.Picket
 import ru.nucodelabs.gem.util.fx.getValue
 import ru.nucodelabs.gem.util.fx.setValue
 import ru.nucodelabs.gem.util.fx.toObservableList
 import ru.nucodelabs.gem.view.AbstractController
+import ru.nucodelabs.gem.view.control.chart.log.LogarithmicAxis
+import ru.nucodelabs.gem.view.tables.DEFAULT_FONT_SIZE
+import ru.nucodelabs.gem.view.tables.STYLE_FOR_FIXED
+import ru.nucodelabs.geo.ves.Picket
+import ru.nucodelabs.geo.ves.Section
 import java.net.URL
 import java.text.DecimalFormat
 import java.util.*
@@ -34,7 +36,10 @@ import javax.inject.Inject
 class NormalizationScreenController @Inject constructor(
     private val observableSection: ObservableSection,
     _picket: ObservableObjectValue<Picket>,
-    private val decimalFormat: DecimalFormat
+    private val decimalFormat: DecimalFormat,
+    private val numberStringConverter: StringConverter<Number>,
+    private val picketIndex: IntegerProperty,
+    private val historyManager: HistoryManager<Section>
 ) : AbstractController() {
 
     private class FixedMn2Model(
@@ -51,6 +56,12 @@ class NormalizationScreenController @Inject constructor(
     }
 
     @FXML
+    private lateinit var xAxis: LogarithmicAxis
+
+    @FXML
+    private lateinit var yAxis: LogarithmicAxis
+
+    @FXML
     private lateinit var mn2Col: TableColumn<FixedMn2Model, String>
 
     @FXML
@@ -65,6 +76,8 @@ class NormalizationScreenController @Inject constructor(
     @FXML
     private lateinit var root: Stage
 
+    private lateinit var normalizationResult: List<Double>
+
     override val stage: Stage
         get() = root
 
@@ -72,6 +85,10 @@ class NormalizationScreenController @Inject constructor(
 
     override fun initialize(location: URL, resources: ResourceBundle) {
         super.initialize(location, resources)
+
+        xAxis.tickLabelFormatter = numberStringConverter
+        yAxis.tickLabelFormatter = numberStringConverter
+
         observableSection.pickets.addListener(ListChangeListener { c ->
             while (c.next()) {
                 if (root.isShowing) {
@@ -82,7 +99,10 @@ class NormalizationScreenController @Inject constructor(
 
         root.onShown = EventHandler { update() }
 
+        table.selectionModel.selectionMode = SelectionMode.MULTIPLE
+
         setupCellFactories()
+        setupRowFactories()
     }
 
     private fun update() {
@@ -94,8 +114,51 @@ class NormalizationScreenController @Inject constructor(
     private fun setupCellFactories() {
         mn2Col.cellValueFactory =
             Callback { f -> createStringBinding({ decimalFormat.format(f.value.mn2) }, f.value.mn2Property()) }
-        fixedCol.cellValueFactory = Callback { f -> f.value.fixedProperty() }
-        fixedCol.cellFactory = CheckBoxTableCell.forTableColumn(fixedCol)
+        mn2Col.cellFactory =
+            Callback {
+                TextFieldTableCell<FixedMn2Model, String>().apply {
+                    indexProperty().addListener { _, _, _ ->
+                        if (index >= 0 && index <= table.items.lastIndex) {
+                            style = if (table.items[index].isFixed) {
+                                STYLE_FOR_FIXED
+                            } else {
+                                ""
+                            }
+                            table.items[index].fixedProperty().addListener { _, _, fix ->
+                                style = if (fix) {
+                                    STYLE_FOR_FIXED
+                                } else {
+                                    ""
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun setupRowFactories() {
+        table.rowFactory = Callback {
+            TableRow<FixedMn2Model>().apply {
+                val contextMenu = ContextMenu(
+                    MenuItem("Зафиксировать").apply {
+                        onAction = EventHandler { fixSelected() }
+                    },
+                    MenuItem("Разблокировать").apply {
+                        onAction = EventHandler { unfixSelected() }
+                    }
+                ).apply { style = "-fx-font-size: $DEFAULT_FONT_SIZE;" }
+                onContextMenuRequested = EventHandler { contextMenu.show(this, it.screenX, it.screenY) }
+            }
+        }
+    }
+
+    private fun unfixSelected() {
+        table.selectionModel.selectedItems.forEach { it.isFixed = false }
+    }
+
+    private fun fixSelected() {
+        table.selectionModel.selectedItems.forEach { it.isFixed = true }
     }
 
     private fun mapItems() {
@@ -134,7 +197,9 @@ class NormalizationScreenController @Inject constructor(
                 picket.sortedExperimentalData,
                 table.items.map { FixableValue(it.mn2, it.isFixed) },
                 idxMap
-            ).mapIndexed { i, resApp ->
+            ).also {
+                normalizationResult = it
+            }.mapIndexed { i, resApp ->
                 XYChart.Data(picket.sortedExperimentalData[i].ab2 as Number, resApp as Number)
             }.toObservableList()
         )
@@ -146,6 +211,14 @@ class NormalizationScreenController @Inject constructor(
     }
 
     fun apply() {
-        table.items[0].isFixed = true
+        val newExp = picket.sortedExperimentalData.mapIndexed { idx, exp ->
+            exp.copy(resistanceApparent = normalizationResult[idx])
+        }
+
+        historyManager.snapshotAfter {
+            observableSection.pickets[picketIndex.value] = picket.copy(experimentalData = newExp)
+        }
+
+        stage.close()
     }
 }
