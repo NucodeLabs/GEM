d336e3f7dee9602d8e789f9ddc94d25a10258131
Shreksatronik <a.ordina@g.nsu.ru>
2023-03-06 00:54:10 +0700

    text inside polygon

--- src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
@@ -2,12 +2,17 @@ package ru.nucodelabs.gem.view.control.chart
 
 import javafx.beans.NamedArg
 import javafx.collections.ObservableList
+import javafx.geometry.Pos
 import javafx.scene.Group
 import javafx.scene.Node
 import javafx.scene.chart.ValueAxis
+import javafx.scene.control.Label
 import javafx.scene.layout.StackPane
+import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.text.Text
+import smile.vq.Neighborhood.bubble
+
 
 class PolygonWithNamesChart (
     @NamedArg("xAxis") xAxis: ValueAxis<Number>,
@@ -18,13 +23,16 @@ class PolygonWithNamesChart (
 
    private fun updateText(): ObservableList<Node>? {
        for ((s,p) in seriesPolygons) {
-           val po = p.points
-           val text = Text(po[0], po[1], s.name)
-            text.font = Font(15.0)
            val group = Group()
-           group.children.addAll(text)
            group.children.addAll(p)
            plotChildren.addAll(group)
+           val po = p.points
+           val text = Text(po[0], po[1]+15, s.name)
+           text.font = Font(17.0)
+           text.fill = Color.WHITE
+           text.strokeWidth = 0.6
+           text.stroke = Color.BLACK
+           group.children.addAll(text)
        }
        return plotChildren
    }
