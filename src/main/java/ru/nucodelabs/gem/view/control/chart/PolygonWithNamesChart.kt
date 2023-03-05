96e229f4ae66a8d43c4133b355fed1e4ec4bbb30
lilvadim <vadimmos2002@gmail.com>
2023-03-06 01:20:27 +0700

    shadow instead of stroke + map property added

--- src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
@@ -1,41 +1,43 @@
 package ru.nucodelabs.gem.view.control.chart
 
 import javafx.beans.NamedArg
-import javafx.collections.ObservableList
-import javafx.geometry.Pos
 import javafx.scene.Group
-import javafx.scene.Node
 import javafx.scene.chart.ValueAxis
-import javafx.scene.control.Label
-import javafx.scene.layout.StackPane
+import javafx.scene.effect.DropShadow
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.text.Text
-import smile.vq.Neighborhood.bubble
 
 
-class PolygonWithNamesChart (
+class PolygonWithNamesChart(
     @NamedArg("xAxis") xAxis: ValueAxis<Number>,
-    @NamedArg("yAxis") yAxis: ValueAxis<Number>) : PolygonChart(xAxis, yAxis) {
+    @NamedArg("yAxis") yAxis: ValueAxis<Number>
+) : PolygonChart(xAxis, yAxis) {
     init {
         animated = false
     }
 
-   private fun updateText(): ObservableList<Node>? {
-       for ((s,p) in seriesPolygons) {
-           val group = Group()
-           group.children.addAll(p)
-           plotChildren.addAll(group)
-           val po = p.points
-           val text = Text(po[0], po[1]+15, s.name)
-           text.font = Font(17.0)
-           text.fill = Color.WHITE
-           text.strokeWidth = 0.6
-           text.stroke = Color.BLACK
-           group.children.addAll(text)
-       }
-       return plotChildren
-   }
+    val seriesText: Map<Series<Number, Number>, Text>
+        get() = _seriesText
+
+    private val _seriesText = mutableMapOf<Series<Number, Number>, Text>()
+
+    private fun updateText() {
+        for ((series, polygon) in seriesPolygons) {
+            val group = Group()
+            group.children.addAll(polygon)
+            plotChildren.addAll(group)
+            val points = polygon.points
+            val text = Text(points[0] + 3, points[1] + 10, series.name).apply {
+                font = Font(11.0)
+                fill = Color.WHITE
+                effect = DropShadow(2.0, Color.BLACK)
+                textProperty().bind(series.nameProperty())
+            }
+            group.children += text
+            _seriesText[series] = text
+        }
+    }
 
 
     override fun layoutPlotChildren() {
@@ -52,7 +54,6 @@ class PolygonWithNamesChart (
         super.seriesAdded(series, seriesIndex)
         updateText()
     }
-
 }
 
 
