7f537b631391f4588dc1236c6d08b75332ce78c8
kymaka <seafth345@gmail.com>
2023-03-16 22:50:26 +0700

    Methods to find red line on image and calculate extra space in meters + changes in satImage func.

--- src/main/java/ru/nucodelabs/gem/view/controller/charts/AnisotropyMapImageProvider.kt
+++ src/main/java/ru/nucodelabs/gem/view/controller/charts/AnisotropyMapImageProvider.kt
@@ -1,9 +1,12 @@
 package ru.nucodelabs.gem.view.controller.charts
 
 import javafx.scene.image.Image
+import javafx.scene.image.WritableImage
+import javafx.scene.paint.Color
 import ru.nucodelabs.gem.net.MapImageProvider
-import ru.nucodelabs.geo.map.MapSizer
+import ru.nucodelabs.geo.map.*
 import javax.inject.Inject
+import kotlin.math.abs
 
 class AnisotropyMapImageProvider @Inject constructor(
     val mapImageProvider: MapImageProvider
@@ -15,12 +18,55 @@ class AnisotropyMapImageProvider @Inject constructor(
     fun satImage(mapSizer: MapSizer): Image {
         val bottomLeft = mapSizer.bottomLeftCorner
         val upperRight = mapSizer.upperRightCorner
-        val stream = mapImageProvider.requestImage(
+        val upperLeft = mapSizer.center + Offset(
+            -1 * mapSizer.maxAbsXFromCenterInMeters,
+            mapSizer.maxAbsYFromCenterInMeters
+            )
+        val imageWithLine = mapImageProvider.requestImage(
             lonBottomLeft = bottomLeft.longitudeInDegrees,
             latBottomLeft = bottomLeft.latitudeInDegrees,
             lonUpperRight = upperRight.longitudeInDegrees,
-            latUpperRight = upperRight.latitudeInDegrees
+            latUpperRight = upperRight.latitudeInDegrees,
+            lonUpperLeft = upperLeft.longitudeInDegrees,
+            latUpperLeft = upperLeft.latitudeInDegrees,
+            width = 450,
+            height = 450
         )
-        return Image(stream)
+        //val extraDist = imageParser(Image(imageWithLine), mapSizer.maxAbsXFromCenterInMeters)
+/*        val stream = mapImageProvider.requestImage(
+            lonBottomLeft = bottomLeft.longitudeInDegrees,
+            latBottomLeft = bottomLeft.latitudeInDegrees,
+            lonUpperRight = upperRight.longitudeInDegrees,
+            latUpperRight = upperRight.latitudeInDegrees,
+            width = 450,
+            height = 450
+        )*/
+        return Image(imageWithLine)
+    }
+
+    private fun imageParser(image: Image, maxDist: Double): Double {
+        val pixelReader = image.pixelReader
+        var pixelCount = 0
+        var endX = 0
+        var endOfLine = false
+        for (y in 0 until image.height.toInt()) {
+            for (x in 0 until image.width.toInt()) {
+                while (Color.web(pixelReader.getArgb(x, y).toString()) == Color.RED) {
+                    pixelCount++
+                    endOfLine = true
+                }
+                if (endOfLine) {
+                    endX = x
+                }
+            }
+            if (endOfLine) break
+        }
+        return pixelsToMeters(pixelCount, endX, maxDist)
+    }
+
+    private fun pixelsToMeters(pixelCount: Int, xCord: Int, maxDist: Double): Double {
+        val extraInPixels = 450 - xCord
+        val metersInPixel = pixelCount.toDouble() / (maxDist * 2.0)
+        return extraInPixels * metersInPixel
     }
 }
