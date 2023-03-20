e26f43d9daad59c334c38eedf505194022c4d027
lilvadim <vadimmos2002@gmail.com>
2023-03-20 22:48:12 +0700

    update

--- src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/PolygonWithNamesChart.kt
@@ -5,7 +5,6 @@ import javafx.scene.Group
 import javafx.scene.chart.ValueAxis
 import javafx.scene.effect.DropShadow
 import javafx.scene.paint.Color
-import javafx.scene.shape.Polygon
 import javafx.scene.text.Font
 import javafx.scene.text.Text
 
@@ -14,76 +13,61 @@ class PolygonWithNamesChart(
     @NamedArg("xAxis") xAxis: ValueAxis<Number>,
     @NamedArg("yAxis") yAxis: ValueAxis<Number>
 ) : PolygonChart(xAxis, yAxis) {
-    init {
-        animated = false
-    }
 
+    /**
+     * Maps series to corresponding Text node
+     */
     val seriesText: Map<Series<Number, Number>, Text>
         get() = _seriesText
 
-    val textGroup: Map<Series<Number,Number>,Group>
-        get() = _group
-
     private val _seriesText = mutableMapOf<Series<Number, Number>, Text>()
-    private val _group = mutableMapOf<Series<Number,Number>,Group>()
 
-    private var flag = false
-
-    private fun removeGroup(series: Series<Number, Number>){
-        plotChildren -= _group[series]
-        _group.keys -= series
-        textRemoved(series)
-    }
-     private fun textRemoved(series: Series<Number, Number>){
-         plotChildren -= _seriesText[series]
-         _seriesText.keys -= series
+    private fun removeTextGroup(series: Series<Number, Number>) {
+        plotChildren -= _seriesText[series]
+        _seriesText -= series
     }
 
     override fun layoutPlotChildren() {
         super.layoutPlotChildren()
-        updateText()
+        setupTextAll()
     }
 
     override fun seriesRemoved(series: Series<Number, Number>) {
         super.seriesRemoved(series)
-        removeGroup(series)
+        removeTextGroup(series)
     }
 
     override fun seriesAdded(series: Series<Number, Number>, seriesIndex: Int) {
         super.seriesAdded(series, seriesIndex)
-        updateText()
+        setupText(series)
     }
 
-    private fun updateText() {
-        var i = 0
-        if (flag){
-            return
+    private fun setupTextAll() {
+        for (series in data) {
+            setupText(series)
         }
-        for ((series, polygon) in seriesPolygons) {
-            val points = polygon.points
-            if (series.name == null){
-                continue
-            }
-            val text = Text(points[0] + 3, points[1] + 10, series.name).apply {
+    }
+
+    private fun setupText(series: Series<Number, Number>) {
+        val polygon = seriesPolygons[series] ?: return
+
+        val xCoord = polygon.points[0] + 3
+        val yCoord = polygon.points[1] + 10
+
+        val text = _seriesText[series]
+        if (text != null) {
+            text.x = xCoord
+            text.y = yCoord
+        } else {
+            val newText = Text(xCoord, yCoord, series.name ?: "").apply {
                 font = Font(11.0)
                 fill = Color.WHITE
                 effect = DropShadow(2.0, Color.BLACK)
                 textProperty().bind(series.nameProperty())
+
             }
-            i++
-            val group = Group()
-            group.children.addAll(polygon)
-            plotChildren.addAll(group)
-            _group[series] = group
-            _group.getOrPut(series){
-                group
-            }.children += text
-            _seriesText.getOrPut(series){
-                text
-            }
-            if (i == seriesPolygons.size){
-                flag = true
-            }
+            plotChildren += newText
+            _seriesText[series] = newText
         }
     }
 }
