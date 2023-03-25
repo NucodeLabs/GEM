4ab3858018edb89cf7f4aebc9872bf2fa86ea253
lilvadim <vadimmos2002@gmail.com>
2023-03-25 14:56:54 +0700

    update dto

--- src/main/java/ru/nucodelabs/gem/file/dto/anisotropy/AzimuthSignalsDto.kt
+++ src/main/java/ru/nucodelabs/gem/file/dto/anisotropy/AzimuthSignalsDto.kt
@@ -1,5 +1,8 @@
 package ru.nucodelabs.gem.file.dto.anisotropy
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class AzimuthSignalsDto(
     val azimuth: Double,
     val signals: List<SignalDto>
--- src/main/java/ru/nucodelabs/gem/file/dto/anisotropy/ModelLayerDto.kt
+++ src/main/java/ru/nucodelabs/gem/file/dto/anisotropy/ModelLayerDto.kt
@@ -1,5 +1,8 @@
 package ru.nucodelabs.gem.file.dto.anisotropy
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ModelLayerDto(
     val power: Double,
     val resistance: Double,
