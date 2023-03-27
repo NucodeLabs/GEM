7390bef3c1b43ce32ae09d0f3648f6965b5e72db
lilvadim <vadimmos2002@gmail.com>
2023-03-28 01:01:59 +0700

    удалил кринж

--- src/main/java/ru/nucodelabs/geo/anisotropy/Signals.kt
+++ src/main/java/ru/nucodelabs/geo/anisotropy/Signals.kt
@@ -1,20 +1,28 @@
 package ru.nucodelabs.geo.anisotropy
 
 import jakarta.validation.Valid
-import ru.nucodelabs.gem.util.std.AutoEquals
 import ru.nucodelabs.geo.anisotropy.calc.processSignals
 
 class Signals(
     signals: List<Signal> = listOf()
-) : AutoEquals() {
-    init {
-        includeInEquals(
-            { sortedSignals }
-        )
-    }
-
+) {
     val sortedSignals: List<@Valid Signal> = processSignals(signals)
 
     val effectiveSignals: List<@Valid Signal>
         get() = sortedSignals.filter { !it.isHidden }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Signals
+
+        if (sortedSignals != other.sortedSignals) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return sortedSignals.hashCode()
+    }
 }
