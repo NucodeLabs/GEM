58beaf5e1e0746c8daa8ddf41541f6373d977f2a
kymaka <seafth345@gmail.com>
2023-04-06 20:37:32 +0700

    WIP! Map is now correctly displayed. Code refactoring is needed.

--- src/main/java/ru/nucodelabs/gem/view/control/chart/ImageScatterChart.kt
+++ src/main/java/ru/nucodelabs/gem/view/control/chart/ImageScatterChart.kt
@@ -1,6 +1,8 @@
 package ru.nucodelabs.gem.view.control.chart
 
+import javafx.application.Platform
 import javafx.beans.NamedArg
+import javafx.beans.binding.Bindings
 import javafx.beans.property.ObjectProperty
 import javafx.beans.property.SimpleObjectProperty
 import javafx.scene.chart.ScatterChart
@@ -14,16 +16,16 @@ import ru.nucodelabs.gem.util.fx.*
 class ImageScatterChart @JvmOverloads constructor(
     @NamedArg("xAxis") private val xAxis: ValueAxis<Number>,
     @NamedArg("yAxis") private val yAxis: ValueAxis<Number>,
-    image: Image = generateImage(256, 256, Color.RED)
+    image: Image = generateImage(450, 450, Color.WHITE)
 ) : ScatterChart<Number, Number>(xAxis, yAxis) {
 
-    private val plotArea = this.lookup(".chart-plot-background") as Region
-    private val _plotBackgroundProperty = plotArea.backgroundProperty()
+    private var plotArea = this.lookup(".chart-plot-background") as Region
+    private var _plotBackgroundProperty = plotArea.backgroundProperty()
     private var plotBackground: Background? by _plotBackgroundProperty
 
     private val _imageProperty: ObjectProperty<Image> = SimpleObjectProperty(image).apply {
         addListener { _, _, newImg: Image? ->
-            setupImage(newImg ?: generateImage(256, 256, Color.RED))
+            setupImage(newImg ?: generateImage(450, 450, Color.WHITE))
         }
     }
 
@@ -42,7 +44,7 @@ class ImageScatterChart @JvmOverloads constructor(
     }
 
     private fun setImageAsBackground(img: Image) {
-        plotBackground = Background(
+        val background = Background(
             BackgroundImage(
                 img,
                 BackgroundRepeat.NO_REPEAT,
@@ -51,6 +53,27 @@ class ImageScatterChart @JvmOverloads constructor(
                 BackgroundSize.DEFAULT
             )
         )
+
+        Platform.runLater {
+            plotArea = this.lookup(".chart-plot-background") as Region
+            _plotBackgroundProperty = plotArea.backgroundProperty()
+/*            plotBackground = Background(
+                BackgroundImage(
+                    img,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.DEFAULT,
+                    BackgroundSize.DEFAULT
+                )
+            )*/
+            _plotBackgroundProperty.bind(
+                Bindings.createObjectBinding(
+                    {
+                        background
+                    }, observableListOf(img)
+                )
+            )
+        }
     }
 
     private fun bindChartSizeToImageSize(img: Image) {
