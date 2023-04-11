49aebf981f6a3d9dc81f4cba2f64a0dc83da2959
lilvadim <vadimmos2002@gmail.com>
2023-04-11 11:35:25 +0700

    fix

--- src/main/java/ru/nucodelabs/gem/app/io/Utils.kt
+++ src/main/java/ru/nucodelabs/gem/app/io/Utils.kt
@@ -2,11 +2,10 @@ package ru.nucodelabs.gem.app.io
 
 import javafx.stage.FileChooser
 import ru.nucodelabs.gem.app.pref.Preference
-import ru.nucodelabs.kfx.core.AbstractViewController
 import java.io.File
 import java.util.prefs.Preferences
 
-fun AbstractViewController<*>.saveInitialDirectory(
+fun saveInitialDirectory(
     preferences: Preferences,
     initDirPref: Preference<String>,
     fileChooser: FileChooser,
