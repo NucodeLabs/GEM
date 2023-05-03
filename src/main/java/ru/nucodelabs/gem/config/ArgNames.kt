e747698434bf5491eab9f0857aee602b6f57c17a
lilvadim <vadimmos2002@gmail.com>
2023-05-04 02:37:44 +0700

    ArgNames.kt

--- src/main/java/ru/nucodelabs/gem/config/ArgNames.kt
+++ src/main/java/ru/nucodelabs/gem/config/ArgNames.kt
@@ -1,25 +1,21 @@
-package ru.nucodelabs.gem.config;
+package ru.nucodelabs.gem.config
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+object ArgNames {
+    const val INITIAL = "initial"
+    const val CSS = "CSS"
+    const val CLR = "CLR"
+    const val PRECISE = "Precise"
+    const val SAVE = "SAVE"
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ArgNames {
-    public static final String INITIAL = "initial";
-    public static final String CSS = "CSS";
-    public static final String CLR = "CLR";
-    public static final String PRECISE = "Precise";
-    public static final String SAVE = "SAVE";
-
-    public static class View {
-        public static final String MAIN_VIEW = "MainView";
-        public static final String ANISOTROPY_MAIN_VIEW = "AnisotropyMainView";
+    object View {
+        const val MAIN_VIEW = "MainView"
+        const val ANISOTROPY_MAIN_VIEW = "AnisotropyMainView"
     }
 
-    public static class File {
-        public static final String JSON = "JSON";
-        public static final String MOD = "MOD";
-        public static final String EXP = "EXP";
-        public static final String PNG = "PNG";
+    object File {
+        const val JSON = "JSON"
+        const val MOD = "MOD"
+        const val EXP = "EXP"
+        const val PNG = "PNG"
     }
 }
