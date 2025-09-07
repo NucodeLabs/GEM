3f64e98b7391a6c59f0f8d54c1583da01b0b630b
Vadim Mostovoy <vadimmos2002@gmail.com>
2025-09-08 00:52:23 +0700

    Add Result.kt helpers

--- common-utils/src/main/kotlin/ru/nucodelabs/util/Result.kt
+++ common-utils/src/main/kotlin/ru/nucodelabs/util/Result.kt
@@ -7,6 +7,7 @@ class Ok<T, E>(val value: T) : Result<T, E>
 class Err<T, E>(val error: E) : Result<T, E>
 
 fun <T, E> T.toOkResult(): Result<T, E> = Ok(this)
+fun <T, E> E.toErrorResult(): Result<T, E> = Err(this)
 
 inline fun <T, E> Result<T, E>.okOrThrow(
     mapError: (E) -> Throwable = { IllegalStateException(it.toString()) }
@@ -17,9 +18,18 @@ inline fun <T, E> Result<T, E>.okOrThrow(
     }
 }
 
-inline fun <T, E> Result<T, E>.onError(onError: (E) -> Unit) {
+inline fun <T, E> Result<T, E>.ifError(onError: (E) -> Unit): Result<T, E> {
     when (this) {
         is Err -> onError(error)
-        else -> return
+        else -> return this
     }
+    return this
+}
+
+inline fun <T, E> Result<T, E>.ifOk(onOk: (T) -> Unit): Result<T, E> {
+    when (this) {
+        is Ok -> onOk(value)
+        else -> return this
+    }
+    return this
 }
