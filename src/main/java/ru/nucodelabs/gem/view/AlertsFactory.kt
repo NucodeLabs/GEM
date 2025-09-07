951315a6793a6ceeff1fa12a924e9083b761d883
Vadim Mostovoy <vadimmos2002@gmail.com>
2025-09-08 00:28:56 +0700

    Add logging in AlertsFactory.kt

--- src/main/java/ru/nucodelabs/gem/view/AlertsFactory.kt
+++ src/main/java/ru/nucodelabs/gem/view/AlertsFactory.kt
@@ -4,11 +4,14 @@ import jakarta.inject.Inject
 import jakarta.validation.ConstraintViolation
 import javafx.scene.control.Alert
 import javafx.stage.Stage
+import ru.nucodelabs.gem.config.slf4j
 import ru.nucodelabs.kfx.ext.currentWindow
 import ru.nucodelabs.kfx.ext.get
 import java.util.*
 
 class AlertsFactory @Inject constructor(private val uiProperties: ResourceBundle) {
+
+    val log = slf4j(this)
 
     fun simpleAlert(
         title: String = uiProperties["error"],
@@ -26,7 +29,7 @@ class AlertsFactory @Inject constructor(private val uiProperties: ResourceBundle
         title = uiProperties["error"]
         initOwner(currentWindow())
         headerText = "Сохраните важные данные и перезапустите программу"
-    }
+    }.also { log.warn("Uncaught exception alert", e) }
 
     @JvmOverloads
     fun simpleExceptionAlert(e: Throwable, owner: Stage? = currentWindow()): Alert =
@@ -34,7 +37,7 @@ class AlertsFactory @Inject constructor(private val uiProperties: ResourceBundle
             title = uiProperties["error"]
             headerText = title
             initOwner(owner)
-        }
+        }.also { log.warn("Simple exception alert", e) }
 
     @JvmOverloads
     fun unsatisfiedLinkErrorAlert(e: UnsatisfiedLinkError, owner: Stage? = currentWindow()): Alert =
@@ -42,20 +45,20 @@ class AlertsFactory @Inject constructor(private val uiProperties: ResourceBundle
             title = uiProperties["noLib"]
             headerText = uiProperties["unableToDrawChart"]
             initOwner(owner)
-        }
+        }.also { log.warn("Unsatisfied link error", e) }
 
 
     @JvmOverloads
     fun incorrectFileAlert(e: Exception, owner: Stage? = currentWindow()): Alert =
         simpleExceptionAlert(e, owner).apply {
             headerText = uiProperties["fileError"]
-        }
+        }.also { log.warn("Incorrect file alert", e) }
 
     @JvmOverloads
     fun violationsAlert(violations: Set<ConstraintViolation<*>>, owner: Stage? = currentWindow()): Alert {
         val message = violations.joinToString("\n") { it.message }
         return Alert(Alert.AlertType.ERROR, message).apply {
             initOwner(owner)
-        }
+        }.also { log.warn("Validation violations alert: $message") }
     }
 }
