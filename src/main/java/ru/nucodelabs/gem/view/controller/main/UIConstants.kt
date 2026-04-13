9623e0a61fa98900617aa027157aa81d7b288a3b
Vadim Mostovoy <vadimmos2002@gmail.com>
2026-04-13 18:34:18 +0700

    Fix size issue

--- src/main/java/ru/nucodelabs/gem/view/controller/main/UIConstants.kt
+++ src/main/java/ru/nucodelabs/gem/view/controller/main/UIConstants.kt
@@ -5,4 +5,5 @@ const val COLOR_AXIS_WIDTH = 70
 const val TOP_CHARTS_RIGHT_OFFSET = COLOR_AXIS_WIDTH + 12
 
 const val MIN_SIZE = 50
+const val MIN_SIZE_2X = 2 * MIN_SIZE
 const val MIN_WIN_SIZE = 500
